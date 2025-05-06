47a1fbe059bab321c46c4bc4a8aef161f69818b1
yooookm <yookyungmi02@gmail.com>
2025-05-06 23:35:33 +0900

    feat: 공통 예외 처리 핸들러 구현

---
+++ src/main/java/com/example/lighton/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.example.lighton.common.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
