ff46cdc187e72dc6cc50ad438fa166cddc05c52b
Kyeongmi Yoo <kyeongmiyoo@Kyeongmiui-MacBookPro.local>
2025-05-06 23:13:38 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/example/lighton/LightonApplication.java
@@ -0,0 +1,13 @@
+package com.example.lighton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lighton/LightonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lighton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
