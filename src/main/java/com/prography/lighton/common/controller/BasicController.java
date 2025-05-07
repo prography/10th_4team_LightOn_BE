f09a013ae015388152893b5bf82a61bf18bbe382
msk226 <kminseok5167@gmail.com>
2025-05-07 09:26:17 +0900

    feat: 헬스 체크 API 응답 포맷 통일

--- src/main/java/com/prography/lighton/common/controller/BasicController.java
+++ src/main/java/com/prography/lighton/common/controller/BasicController.java
@@ -4,11 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.prography.lighton.common.utils.ApiUtils;
+import com.prography.lighton.common.utils.ApiUtils.ApiResult;
+
 @RestController
 public class BasicController {
 
 	@GetMapping("/health")
-	public ResponseEntity<String> healthCheck() {
-		return ResponseEntity.ok("OK");
+	public ApiResult<String> healthCheck() {
+		return ApiUtils.success("ok");
 	}
 }
