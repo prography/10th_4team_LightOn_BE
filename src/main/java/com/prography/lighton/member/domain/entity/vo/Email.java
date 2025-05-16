4054847d76192b3aaa75ba32684e9a62c52257c6
msk226 <kminseok5167@gmail.com>
2025-05-16 15:54:16 +0900

    feat: Email VO equals/hashCode 구현

--- src/main/java/com/prography/lighton/member/domain/entity/vo/Email.java
+++ src/main/java/com/prography/lighton/member/domain/entity/vo/Email.java
@@ -1,11 +1,32 @@
 package com.prography.lighton.member.domain.entity.vo;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 
 @Embeddable
 public class Email {
 
     @Column(nullable = false, unique = true)
-    private String email;
+    private String value;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Email)) return false;
+        Email email = (Email) o;
+        return Objects.equals(value, email.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
