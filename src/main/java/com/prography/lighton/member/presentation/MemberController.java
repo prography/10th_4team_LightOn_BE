f2c466b8800e4fcb31b4678304a9ec180919aad3
msk226 <kminseok5167@gmail.com>
2025-05-16 16:24:11 +0900

    feat: 회원 가입 API 제작

--- src/main/java/com/prography/lighton/member/presentation/MemberController.java
+++ src/main/java/com/prography/lighton/member/presentation/MemberController.java
@@ -2,12 +2,15 @@ package com.prography.lighton.member.presentation;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prography.lighton.common.utils.ApiUtils;
 import com.prography.lighton.common.utils.ApiUtils.ApiResult;
+import com.prography.lighton.member.application.RegisterMemberUseCase;
+import com.prography.lighton.member.application.command.RegisterMemberCommand;
 import com.prography.lighton.member.presentation.dto.request.SignUpMemberRequestDTO;
 import com.prography.lighton.member.presentation.dto.response.SignUpMemberResponseDTO;
 
@@ -15,9 +18,15 @@ import com.prography.lighton.member.presentation.dto.response.SignUpMemberRespon
 @RequestMapping("/api/users")
 public class MemberController {
 
+	private final RegisterMemberUseCase registerMemberUseCase;
+
+	public MemberController(RegisterMemberUseCase registerMemberUseCase) {
+		this.registerMemberUseCase = registerMemberUseCase;
+	}
+
 	@PostMapping
-	public ApiResult<SignUpMemberResponseDTO> register(SignUpMemberRequestDTO request) {
-		return ApiUtils.success(null);
+	public ApiResult<SignUpMemberResponseDTO> register(@RequestBody SignUpMemberRequestDTO request) {
+		return ApiUtils.success(registerMemberUseCase.registerMember(RegisterMemberCommand.of(request)));
 	}
 
 	@PostMapping("/login")
