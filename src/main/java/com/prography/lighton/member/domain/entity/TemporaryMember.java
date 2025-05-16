6e0a59c53c6ebb3024f5b084bb0421ede0111753
msk226 <kminseok5167@gmail.com>
2025-05-16 15:52:09 +0900

    feat: 임시 회원 엔티티 정의

---
+++ src/main/java/com/prography/lighton/member/domain/entity/TemporaryMember.java
@@ -0,0 +1,32 @@
+package com.prography.lighton.member.domain.entity;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import com.prography.lighton.common.BaseEntity;
+import com.prography.lighton.member.domain.entity.vo.Email;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE temporary_member SET status = false WHERE id = ?")
+@SQLRestriction("status = true")
+public class TemporaryMember extends BaseEntity {
+
+	@Embedded
+	private Email email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false)
+	@ColumnDefault("false")
+	private Boolean isRegistered = false;
+}
