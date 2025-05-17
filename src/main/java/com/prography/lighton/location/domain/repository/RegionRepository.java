8fa6a21528502b3406bf9f2ef1141bffb66c2097
msk226 <kminseok5167@gmail.com>
2025-05-17 13:47:21 +0900

    refactor: 불필요한 출력문 삭제

--- src/main/java/com/prography/lighton/location/domain/repository/RegionRepository.java
+++ src/main/java/com/prography/lighton/location/domain/repository/RegionRepository.java
@@ -10,7 +10,6 @@ import com.prography.lighton.location.exception.NoSuchRegionException;
 public interface RegionRepository extends JpaRepository<Region, Long> {
 
 	default Region getByRegionCode(Integer code) {
-		System.out.println((long) ((code / 100) * 100));
 		return findById((long) ((code / 100) * 100))
 				.orElseThrow(NoSuchRegionException::new);
 	}
