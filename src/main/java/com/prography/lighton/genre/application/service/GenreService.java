6992bd6d13f6c4f10b8e00c0d01c405af3136853
yooookm <yookyungmi02@gmail.com>
2025-05-18 23:06:06 +0900

    feat: GenreService init() 함수에서 트랜잭션 제거

--- src/main/java/com/prography/lighton/genre/application/service/GenreService.java
+++ src/main/java/com/prography/lighton/genre/application/service/GenreService.java
@@ -11,7 +11,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,8 +21,7 @@ public class GenreService {
     private Map<Long, Genre> cache;
 
     @PostConstruct
-    @Transactional
-    public void initCache() {
+    public void init() {
         this.cache = genreRepository.findAll().stream()
                 .collect(Collectors.toUnmodifiableMap(Genre::getId, Function.identity()));
     }
