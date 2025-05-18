c38c1d8b8c3c0ac332e546c11bdbbfd9ebad7dca
yooookm <yookyungmi02@gmail.com>
2025-05-18 17:28:04 +0900

    feat: vo 정적 팩토리 메서드 패턴 생성

--- src/main/java/com/prography/lighton/artist/domain/entity/vo/History.java
+++ src/main/java/com/prography/lighton/artist/domain/entity/vo/History.java
@@ -3,8 +3,13 @@ package com.prography.lighton.artist.domain.entity.vo;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Embedded;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
 public class History {
 
     @Column(nullable = false)
@@ -12,4 +17,8 @@ public class History {
 
     @Embedded
     private ActivityImages activityImages;
+
+    public static History of(String bio, List<String> imageUrls) {
+        return new History(bio, ActivityImages.of(imageUrls));
+    }
 }
