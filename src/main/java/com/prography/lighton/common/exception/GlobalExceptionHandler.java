8b53a5b659de722479d9381901e1a04d532f05f5
yooookm <yookyungmi02@gmail.com>
2025-05-19 01:08:28 +0900

    feat: HttpMessageNotReadableException에 대한  exception handler 구현 - 잘못된 enum 타입 request가 들어왔을 때 사용

--- src/main/java/com/prography/lighton/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prography/lighton/common/exception/GlobalExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.prography.lighton.common.exception;
 
+import com.fasterxml.jackson.databind.JsonMappingException.Reference;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.prography.lighton.common.exception.base.NotFoundException;
 import com.prography.lighton.common.utils.ApiUtils;
 import com.prography.lighton.member.exception.InvalidMemberException;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -36,6 +40,20 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ApiUtils.error(HttpStatus.BAD_REQUEST, e.getFieldError().getDefaultMessage()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ApiUtils.ApiResult<Void> handleEnumBindingError(HttpMessageNotReadableException ex) {
+        if (ex.getCause() instanceof InvalidFormatException e) {
+            String field = e.getPath().stream()
+                    .map(Reference::getFieldName)
+                    .collect(Collectors.joining("."));
+
+            String message = String.format("'%s'은(는) 필드 '%s'에 허용되지 않는 값입니다.", e.getValue(), field);
+            return ApiUtils.error(HttpStatus.BAD_REQUEST, message);
+        }
+
+        return ApiUtils.error(HttpStatus.BAD_REQUEST, "요청 데이터를 읽을 수 없습니다.");
     }
 
     @ExceptionHandler(Exception.class)
