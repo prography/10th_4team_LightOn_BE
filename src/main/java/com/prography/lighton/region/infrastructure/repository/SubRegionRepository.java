0a3aad78798d2af8ac43f6149a5a13805ae1d2f8
yooookm <yookyungmi02@gmail.com>
2025-05-18 23:07:33 +0900

    feat: 소분류 지역 조회 시 대분류 지역까지 fetch join 조회하는 함수 구현

--- src/main/java/com/prography/lighton/region/infrastructure/repository/SubRegionRepository.java
+++ src/main/java/com/prography/lighton/region/infrastructure/repository/SubRegionRepository.java
@@ -1,16 +1,18 @@
 package com.prography.lighton.region.infrastructure.repository;
 
 import com.prography.lighton.region.domain.entity.SubRegion;
-import com.prography.lighton.region.exception.NoSuchRegionException;
-import java.util.Optional;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SubRegionRepository extends JpaRepository<SubRegion, Long> {
 
-    Optional<SubRegion> findByCode(Integer code);
-
-    default SubRegion getByRegionCode(Integer code) {
-        return findByCode(code)
-                .orElseThrow(NoSuchRegionException::new);
-    }
+    @Query("""
+                SELECT sr
+                FROM SubRegion sr
+                JOIN FETCH sr.region r
+                WHERE sr.status = true
+                  AND r.status  = true
+            """)
+    List<SubRegion> findAllActiveWithRegion();
 }
