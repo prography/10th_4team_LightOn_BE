86e5625d9f5f65bed1714b77ef4d471925899194
yooookm <yookyungmi02@gmail.com>
2025-05-19 03:30:31 +0900

    feat: 공연 등록 기능 구현

--- src/main/java/com/prography/lighton/performance/application/service/PerformanceService.java
+++ src/main/java/com/prography/lighton/performance/application/service/PerformanceService.java
@@ -1,6 +1,13 @@
 package com.prography.lighton.performance.application.service;
 
+import com.prography.lighton.artist.application.service.ArtistService;
+import com.prography.lighton.artist.domain.entity.Artist;
+import com.prography.lighton.member.domain.entity.Member;
+import com.prography.lighton.performance.application.resolver.PerformanceResolver;
+import com.prography.lighton.performance.domain.entity.Performance;
 import com.prography.lighton.performance.infrastructure.repository.PerformanceRepository;
+import com.prography.lighton.performance.presentation.dto.PerformanceRegisterRequest;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +16,13 @@ import org.springframework.stereotype.Service;
 public class PerformanceService {
 
     private final PerformanceRepository performanceRepository;
+    private final PerformanceResolver performanceResolver;
+    private final ArtistService artistService;
+
+    @Transactional
+    public void registerPerformance(Member member, PerformanceRegisterRequest request) {
+        Artist artist = artistService.getApprovedArtistByMember(member);
+        Performance performance = performanceResolver.resolve(artist, request);
+        performanceRepository.save(performance);
+    }
 }
