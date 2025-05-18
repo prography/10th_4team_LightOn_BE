e620068d18977ad6ccde54b1e37b1c6877679a74
yooookm <yookyungmi02@gmail.com>
2025-05-18 17:43:47 +0900

    feat: db에서 member로 아티스트 찾기 함수 작성

--- src/main/java/com/prography/lighton/artist/infrastructure/repository/ArtistRepository.java
+++ src/main/java/com/prography/lighton/artist/infrastructure/repository/ArtistRepository.java
@@ -1,10 +1,13 @@
 package com.prography.lighton.artist.infrastructure.repository;
 
 import com.prography.lighton.artist.domain.entity.Artist;
+import com.prography.lighton.member.domain.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
+    Optional<Artist> findByMember(Member member);
 }
