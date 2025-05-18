d76c24f243ac5d059428b75745b5a8e2fced1cd5
yooookm <yookyungmi02@gmail.com>
2025-05-19 01:44:33 +0900

    feat: 유료 공연 필드 유효성 관련 커스텀 익셉션 생성

---
+++ src/main/java/com/prography/lighton/performance/domain/entity/exception/InvalidPaymentInfoException.java
@@ -0,0 +1,16 @@
+package com.prography.lighton.performance.domain.entity.exception;
+
+import com.prography.lighton.common.exception.base.InvalidException;
+
+public class InvalidPaymentInfoException extends InvalidException {
+
+    private static final String MESSAGE = "유료 공연일 경우 계좌 정보, 은행명, 예금주, 금액은 모두 필수입니다.";
+
+    public InvalidPaymentInfoException() {
+        super(MESSAGE);
+    }
+
+    public InvalidPaymentInfoException(String message) {
+        super(message);
+    }
+}
