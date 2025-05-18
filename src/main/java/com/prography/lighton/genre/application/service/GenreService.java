4e7e4c450410eb2050ee2074b7d4a47708b58e00
yooookm <yookyungmi02@gmail.com>
2025-05-18 22:50:17 +0900

    feat: 서버 시작 시 장르 정보를 캐시에 저장

--- src/main/java/com/prography/lighton/genre/application/service/GenreService.java
+++ src/main/java/com/prography/lighton/genre/application/service/GenreService.java
@@ -3,9 +3,15 @@ package com.prography.lighton.genre.application.service;
 import com.prography.lighton.genre.application.exception.NoSuchGenreException;
 import com.prography.lighton.genre.domain.entity.Genre;
 import com.prography.lighton.genre.infrastructure.repository.GenreRepository;
+import jakarta.annotation.PostConstruct;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,13 +19,19 @@ public class GenreService {
 
     private final GenreRepository genreRepository;
 
+    private Map<Long, Genre> cache;
+
+    @PostConstruct
+    @Transactional
+    public void initCache() {
+        this.cache = genreRepository.findAll().stream()
+                .collect(Collectors.toUnmodifiableMap(Genre::getId, Function.identity()));
+    }
+
     public List<Genre> getGenresOrThrow(List<Long> genreIds) {
-        List<Genre> genres = genreRepository.findAllById(genreIds);
-
-        if (genres.size() != genreIds.size()) {
-            throw new NoSuchGenreException();
-        }
-
-        return genres;
+        return genreIds.stream()
+                .map(id -> Optional.ofNullable(cache.get(id))
+                        .orElseThrow(NoSuchGenreException::new))
+                .toList();
     }
 }
