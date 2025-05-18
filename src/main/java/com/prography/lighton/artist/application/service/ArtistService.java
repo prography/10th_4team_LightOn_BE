d0e22ae6c41e921638771f8867756ea54104cd8a
yooookm <yookyungmi02@gmail.com>
2025-05-18 17:28:16 +0900

    feat: 아티스트 생성 로직 작성

--- src/main/java/com/prography/lighton/artist/application/service/ArtistService.java
+++ src/main/java/com/prography/lighton/artist/application/service/ArtistService.java
@@ -1,7 +1,12 @@
 package com.prography.lighton.artist.application.service;
 
+import com.prography.lighton.artist.domain.entity.Artist;
+import com.prography.lighton.artist.domain.entity.vo.History;
 import com.prography.lighton.artist.infrastructure.repository.ArtistRepository;
 import com.prography.lighton.artist.presentation.dto.ArtistRegisterRequest;
+import com.prography.lighton.common.vo.RegionInfo;
+import com.prography.lighton.member.domain.entity.Member;
+import com.prography.lighton.region.domain.resolver.RegionResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,9 +17,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class ArtistService {
 
     private final ArtistRepository artistRepository;
+    private final RegionResolver regionResolver;
 
     @Transactional
-    public void registerMember(ArtistRegisterRequest request) {
+    public void registerMember(Member member, ArtistRegisterRequest request) {
+        RegionInfo activityRegion = regionResolver.resolve(request.artist().activityLocation());
+        History history = History.of(request.history().bio(), request.history().activityPhotos());
 
+        Artist artist = Artist.create(
+                member,
+                request.artist().name(),
+                request.artist().description(),
+                activityRegion,
+                history,
+                request.proof()
+        );
+
+        artistRepository.save(artist);
     }
 }
