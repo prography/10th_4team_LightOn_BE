412414d7e2dbd1b4b18841fa3a468d0666c96046
yooookm <yookyungmi02@gmail.com>
2025-05-19 05:20:35 +0900

    feat: 공연 수정 api 구현

--- src/main/java/com/prography/lighton/performance/presentation/controller/ArtistPerformanceController.java
+++ src/main/java/com/prography/lighton/performance/presentation/controller/ArtistPerformanceController.java
@@ -5,9 +5,12 @@ import com.prography.lighton.common.utils.ApiUtils.ApiResult;
 import com.prography.lighton.member.domain.entity.Member;
 import com.prography.lighton.performance.application.service.PerformanceService;
 import com.prography.lighton.performance.presentation.dto.PerformanceRegisterRequest;
+import com.prography.lighton.performance.presentation.dto.PerformanceUpdateRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +27,17 @@ public class ArtistPerformanceController {
         // 나중에 수정 필요
         Member member = Member.withId(1L);
         performanceService.registerPerformance(member, request);
-        return ResponseEntity.ok()
+        return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiUtils.success());
     }
 
+    @PostMapping("/{id}")
+    public ResponseEntity<ApiResult<String>> updatePerformance(@PathVariable Long id,
+                                                               @Valid PerformanceUpdateRequest request) {
+        // 나중에 수정 필요
+        Member member = Member.withId(1L);
+        performanceService.updatePerformance(member, id, request);
+        return ResponseEntity.ok()
+                .body(ApiUtils.success());
+    }
 }
