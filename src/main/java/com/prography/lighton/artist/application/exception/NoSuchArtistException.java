205667698873bed6a77869d3f13f9831e250ee5b
yooookm <yookyungmi02@gmail.com>
2025-05-18 21:57:54 +0900

    feat: 아티스트 존재 오류 커스텀 익셉션 생성

---
+++ src/main/java/com/prography/lighton/artist/application/exception/NoSuchArtistException.java
@@ -0,0 +1,17 @@
+package com.prography.lighton.artist.application.exception;
+
+import com.prography.lighton.common.exception.base.NotFoundException;
+
+public class NoSuchArtistException extends NotFoundException {
+
+    private static final String MESSAGE = "존재하지 않는 아티스트입니다.";
+
+    public NoSuchArtistException() {
+        super(MESSAGE);
+    }
+
+    public NoSuchArtistException(String message) {
+        super(message);
+    }
+
+}
