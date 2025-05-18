8435709a10d4eab53ba56ee9495d7f07b6c86a77
yooookm <yookyungmi02@gmail.com>
2025-05-18 17:28:04 +0900

    feat: vo 정적 팩토리 메서드 패턴 생성

--- src/main/java/com/prography/lighton/artist/domain/entity/vo/ActivityImages.java
+++ src/main/java/com/prography/lighton/artist/domain/entity/vo/ActivityImages.java
@@ -2,8 +2,13 @@ package com.prography.lighton.artist.domain.entity.vo;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
 public class ActivityImages {
     @Column(nullable = false)
     private String imageUrl1;
@@ -15,4 +20,12 @@ public class ActivityImages {
     private String imageUrl4;
 
     private String imageUrl5;
+
+    public static ActivityImages of(List<String> urls) {
+        String[] images = new String[5];
+        for (int i = 0; i < 5; i++) {
+            images[i] = (i < urls.size()) ? urls.get(i) : null;
+        }
+        return new ActivityImages(images[0], images[1], images[2], images[3], images[4]);
+    }
 }
