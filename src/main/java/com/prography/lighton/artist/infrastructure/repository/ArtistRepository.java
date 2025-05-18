e07159cdb144289323137bafb9b545af4ff36f89
yooookm <yookyungmi02@gmail.com>
2025-05-18 23:06:39 +0900

    fix: 아티스트 장르 fetch join 함수 쿼리 수정

--- src/main/java/com/prography/lighton/artist/infrastructure/repository/ArtistRepository.java
+++ src/main/java/com/prography/lighton/artist/infrastructure/repository/ArtistRepository.java
@@ -4,8 +4,9 @@ import com.prography.lighton.artist.application.exception.NoSuchArtistException;
 import com.prography.lighton.artist.domain.entity.Artist;
 import com.prography.lighton.member.domain.entity.Member;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -13,11 +14,18 @@ public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
     Optional<Artist> findByMember(Member member);
 
-    @EntityGraph(attributePaths = "genres.genre")
-    Optional<Artist> findWithGenreMember(Member member);
+    @Query("""
+                select a from Artist a
+                join fetch a.genres ag
+                join fetch ag.genre g
+                where a.member = :member
+                  and a.status = true
+                  and g.status = true
+            """)
+    Optional<Artist> findWithGenresByMember(@Param("member") Member member);
 
     default Artist getByMember(Member member) {
-        return findWithGenreMember(member)
+        return findWithGenresByMember(member)
                 .orElseThrow(NoSuchArtistException::new);
     }
 }
