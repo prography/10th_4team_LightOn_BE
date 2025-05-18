bc6fcf55a7a728b733942c9bf5dd3bbf0d5bd6f4
yooookm <yookyungmi02@gmail.com>
2025-05-18 17:01:11 +0900

    feat: 지역 코드 관련 공통 vo 작성

---
+++ src/main/java/com/prography/lighton/common/vo/RegionInfo.java
@@ -0,0 +1,27 @@
+package com.prography.lighton.common.vo;
+
+import com.prography.lighton.region.domain.entity.Region;
+import com.prography.lighton.region.domain.entity.SubRegion;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class RegionInfo {
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Region region;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private SubRegion subRegion;
+
+    public static RegionInfo of(Region region, SubRegion subRegion) {
+        return new RegionInfo(region, subRegion);
+    }
+}
