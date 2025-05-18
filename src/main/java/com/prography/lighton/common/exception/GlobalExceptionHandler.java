096605d9c4d5314e7e126f702ef4276ec2ab8ce1
yooookm <yookyungmi02@gmail.com>
2025-05-18 21:21:11 +0900

    chore: GlobalExceptionHandler에서 불필요한 import문 삭제

--- src/main/java/com/prography/lighton/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/prography/lighton/common/exception/GlobalExceptionHandler.java
@@ -1,55 +1,47 @@
 package com.prography.lighton.common.exception;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.prography.lighton.common.exception.base.NotFoundException;
+import com.prography.lighton.common.utils.ApiUtils;
+import com.prography.lighton.member.exception.InvalidMemberException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.prography.lighton.common.exception.base.NotFoundException;
-import com.prography.lighton.common.utils.ApiUtils;
-import com.prography.lighton.common.utils.ApiUtils.ApiResult;
-import com.prography.lighton.region.exception.NoSuchRegionException;
-import com.prography.lighton.member.exception.InvalidMemberException;
-import com.prography.lighton.member.exception.NoSuchMemberException;
-
-import jakarta.persistence.EntityNotFoundException;
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(NotFoundException.class)
-	public ResponseEntity<?> handleNotFoundException(NotFoundException e) {
-		return ResponseEntity.status(e.status()).body(ApiUtils.error(e.status(), e.getMessage()));
-	}
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity.status(e.status()).body(ApiUtils.error(e.status(), e.getMessage()));
+    }
 
-	@ExceptionHandler({
-			InvalidMemberException.class
-	})
-	public ResponseEntity<?> handleInvalidMemberException(InvalidMemberException e) {
-		return ResponseEntity.status(e.status()).body(ApiUtils.error(e.status(), e.getMessage()));
-	}
+    @ExceptionHandler({
+            InvalidMemberException.class
+    })
+    public ResponseEntity<?> handleInvalidMemberException(InvalidMemberException e) {
+        return ResponseEntity.status(e.status()).body(ApiUtils.error(e.status(), e.getMessage()));
+    }
 
-	@ExceptionHandler(IllegalArgumentException.class)
-	public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(ApiUtils.error(HttpStatus.BAD_REQUEST, e.getMessage()));
-	}
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiUtils.error(HttpStatus.BAD_REQUEST, e.getMessage()));
+    }
 
-	@ExceptionHandler({MethodArgumentNotValidException.class})
-	public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-				.body(ApiUtils.error(HttpStatus.BAD_REQUEST, e.getFieldError().getDefaultMessage()));
-	}
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiUtils.error(HttpStatus.BAD_REQUEST, e.getFieldError().getDefaultMessage()));
+    }
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> handleException(Exception e) {
-		log.error(e.getMessage(), e);
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-				.body(ApiUtils.error(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다."));
-	}
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiUtils.error(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류가 발생했습니다."));
+    }
 }
