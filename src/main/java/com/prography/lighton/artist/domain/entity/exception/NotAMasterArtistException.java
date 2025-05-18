38e1c67fb9246f567887cfa29d68897edb2df5f5
yooookm <yookyungmi02@gmail.com>
2025-05-19 05:14:08 +0900

    chore: NotAMasterArtistException http code와 메세지 변경

--- src/main/java/com/prography/lighton/artist/domain/entity/exception/NotAMasterArtistException.java
+++ src/main/java/com/prography/lighton/artist/domain/entity/exception/NotAMasterArtistException.java
@@ -4,13 +4,13 @@ import com.prography.lighton.common.utils.ApiUtils;
 import org.springframework.http.HttpStatus;
 
 public class NotAMasterArtistException extends RuntimeException {
-    private final String message = "공연 등록 아티스트가 아닙입니다.";
+    private final String message = "공연을 등록한 아티스트가 아닙니다.";
 
     public ApiUtils.ApiResult<?> body() {
-        return ApiUtils.error(HttpStatus.CONFLICT, message);
+        return ApiUtils.error(HttpStatus.FORBIDDEN, message);
     }
 
     public HttpStatus status() {
-        return HttpStatus.CONFLICT;
+        return HttpStatus.FORBIDDEN;
     }
 }
