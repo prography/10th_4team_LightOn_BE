ced5a454f7b44addbf2027a7b28e6355b4b63111
yooookm <yookyungmi02@gmail.com>
2025-05-19 03:46:35 +0900

    feat: 승인된 공연이 아닐 때 발생하는 커스텀 익셉션 핸들러 처리

--- src/main/java/com/prography/lighton/performance/presentation/exception/PerformanceExceptionHandler.java
+++ src/main/java/com/prography/lighton/performance/presentation/exception/PerformanceExceptionHandler.java
@@ -1,7 +1,17 @@
 package com.prography.lighton.performance.presentation.exception;
 
+import com.prography.lighton.common.utils.ApiUtils.ApiResult;
+import com.prography.lighton.performance.domain.exeption.PerformanceNotApprovedException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class PerformanceExceptionHandler {
+
+    @ExceptionHandler(PerformanceNotApprovedException.class)
+    public ResponseEntity<ApiResult<?>> performanceNotApprovedException(
+            PerformanceNotApprovedException exception) {
+        return new ResponseEntity<>(exception.body(), exception.status());
+    }
 }
