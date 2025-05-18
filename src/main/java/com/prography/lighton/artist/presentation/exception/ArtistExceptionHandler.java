5c573031f671d1d6c68bd4ae08b29e75c40a8193
yooookm <yookyungmi02@gmail.com>
2025-05-18 18:26:40 +0900

    feat: 아티스트 관련 익셉션 핸들러 생성

---
+++ src/main/java/com/prography/lighton/artist/presentation/exception/ArtistExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.prography.lighton.artist.presentation.exception;
+
+import com.prography.lighton.artist.application.exception.ArtistRegistrationNotAllowedException;
+import com.prography.lighton.common.utils.ApiUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ArtistExceptionHandler {
+
+    @ExceptionHandler(ArtistRegistrationNotAllowedException.class)
+    public ResponseEntity<ApiUtils.ApiResult<?>> artistRegistrationNotAllowedException(
+            ArtistRegistrationNotAllowedException exception) {
+        return new ResponseEntity<>(exception.body(), exception.status());
+    }
+}
