9dfaa84fd6979fa8fa3b7a7354fc847a7b97d033
yooookm <yookyungmi02@gmail.com>
2025-05-19 06:37:27 +0900

    chore: 공연 수정 api http method 변경

--- src/main/java/com/prography/lighton/performance/presentation/controller/ArtistPerformanceController.java
+++ src/main/java/com/prography/lighton/performance/presentation/controller/ArtistPerformanceController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,7 @@ public class ArtistPerformanceController {
                 .body(ApiUtils.success());
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<ApiResult<String>> updatePerformance(@PathVariable Long id,
                                                                @Valid @RequestBody PerformanceUpdateRequest request) {
         // 나중에 수정 필요
