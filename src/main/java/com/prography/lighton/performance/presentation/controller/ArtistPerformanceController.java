5e9c85e821e484e85dcaf7bbaf04bcc0bdd5926c
yooookm <yookyungmi02@gmail.com>
2025-05-19 03:31:52 +0900

    feat: 공연 등록 api 작성

--- src/main/java/com/prography/lighton/performance/presentation/controller/ArtistPerformanceController.java
+++ src/main/java/com/prography/lighton/performance/presentation/controller/ArtistPerformanceController.java
@@ -2,6 +2,8 @@ package com.prography.lighton.performance.presentation.controller;
 
 import com.prography.lighton.common.utils.ApiUtils;
 import com.prography.lighton.common.utils.ApiUtils.ApiResult;
+import com.prography.lighton.member.domain.entity.Member;
+import com.prography.lighton.performance.application.service.PerformanceService;
 import com.prography.lighton.performance.presentation.dto.PerformanceRegisterRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/artists/performances")
 public class ArtistPerformanceController {
 
+    private final PerformanceService performanceService;
+
     @PostMapping
     public ResponseEntity<ApiResult<String>> registerPerformance(@Valid PerformanceRegisterRequest request) {
-
+        // 나중에 수정 필요
+        Member member = Member.withId(1L);
+        performanceService.registerPerformance(member, request);
         return ResponseEntity.ok()
                 .body(ApiUtils.success());
     }
