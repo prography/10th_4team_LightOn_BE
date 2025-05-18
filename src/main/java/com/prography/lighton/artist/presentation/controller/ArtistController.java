09e8adf482a5874016822fd2ab05c75f963e4842
yooookm <yookyungmi02@gmail.com>
2025-05-18 18:26:07 +0900

    feat: 회원가입 api 구현

--- src/main/java/com/prography/lighton/artist/presentation/controller/ArtistController.java
+++ src/main/java/com/prography/lighton/artist/presentation/controller/ArtistController.java
@@ -1,9 +1,12 @@
 package com.prography.lighton.artist.presentation.controller;
 
+import com.prography.lighton.artist.application.service.ArtistService;
 import com.prography.lighton.artist.presentation.dto.ArtistRegisterRequest;
 import com.prography.lighton.common.utils.ApiUtils;
 import com.prography.lighton.common.utils.ApiUtils.ApiResult;
+import com.prography.lighton.member.domain.entity.Member;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/artists")
 public class ArtistController {
 
+    private final ArtistService artistService;
+
     @PostMapping
     public ResponseEntity<ApiResult<String>> registerArtist(@Valid @RequestBody ArtistRegisterRequest request) {
+        // 나중에 수정 필요
+        Member member = Member.withId(1L);
+        artistService.registerMember(member, request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiUtils.success());
     }
