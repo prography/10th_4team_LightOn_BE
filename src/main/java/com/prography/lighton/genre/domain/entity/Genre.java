20e5a5c7f4383b4fb20ed77358901036cb652a22
yooookm <yookyungmi02@gmail.com>
2025-05-18 20:09:41 +0900

    feat: 장르 클래스를 엔티티로 변경

--- src/main/java/com/prography/lighton/genre/domain/entity/Genre.java
+++ src/main/java/com/prography/lighton/genre/domain/entity/Genre.java
@@ -1,6 +1,18 @@
 package com.prography.lighton.genre.domain.entity;
 
-public class Genre {
+import com.prography.lighton.common.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE genre SET status = false WHERE id = ?")
+@SQLRestriction("status = true")
+public class Genre extends BaseEntity {
 
     private String name;
 }
