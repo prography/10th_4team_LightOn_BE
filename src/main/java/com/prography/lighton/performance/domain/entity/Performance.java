0344056ce4536ae9d94e1d59f1c27a6094a411db
yooookm <yookyungmi02@gmail.com>
2025-05-19 00:26:23 +0900

    feat: 공연 엔티티에 장르 리스트 필드 추가

--- src/main/java/com/prography/lighton/performance/domain/entity/Performance.java
+++ src/main/java/com/prography/lighton/performance/domain/entity/Performance.java
@@ -4,17 +4,28 @@ import com.prography.lighton.common.BaseEntity;
 import com.prography.lighton.performance.domain.entity.association.PerformanceArtist;
 import com.prography.lighton.performance.domain.entity.enums.Seat;
 import com.prography.lighton.performance.domain.entity.enums.Type;
-import com.prography.lighton.performance.domain.entity.vo.*;
-import jakarta.persistence.*;
+import com.prography.lighton.performance.domain.entity.vo.Info;
+import com.prography.lighton.performance.domain.entity.vo.Location;
+import com.prography.lighton.performance.domain.entity.vo.Payment;
+import com.prography.lighton.performance.domain.entity.vo.Schedule;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -61,4 +72,7 @@ public class Performance extends BaseEntity {
     @Column(nullable = false)
     @ColumnDefault("0")
     private Long likeCount = 0L;
+
+    @OneToMany(mappedBy = "performance", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PerformanceGenre> genres = new ArrayList<>();
 }
