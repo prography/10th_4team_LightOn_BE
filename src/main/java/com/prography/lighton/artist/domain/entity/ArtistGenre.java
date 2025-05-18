87bb06440221591ab2fb69529e08da8c86ba3aef
yooookm <yookyungmi02@gmail.com>
2025-05-18 23:58:56 +0900

    chore: ArtistGenre 필드에 불필요한 어노테이션 제거

--- src/main/java/com/prography/lighton/artist/domain/entity/ArtistGenre.java
+++ src/main/java/com/prography/lighton/artist/domain/entity/ArtistGenre.java
@@ -4,7 +4,6 @@ import com.prography.lighton.common.BaseEntity;
 import com.prography.lighton.genre.domain.entity.Genre;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -21,7 +20,6 @@ public class ArtistGenre extends BaseEntity {
     private Artist artist;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "genre_id")
     private Genre genre;
 
     public static List<ArtistGenre> createListFor(Artist artist, List<Genre> genres) {
