0b1e0618fdfee0dba2b43d28f56dda2476ccf876
yooookm <yookyungmi02@gmail.com>
2025-05-18 21:58:08 +0900

    feat: 아티스트 객체 반환 repository 함수 작성

--- src/main/java/com/prography/lighton/artist/infrastructure/repository/ArtistRepository.java
+++ src/main/java/com/prography/lighton/artist/infrastructure/repository/ArtistRepository.java
@@ -1,5 +1,6 @@
 package com.prography.lighton.artist.infrastructure.repository;
 
+import com.prography.lighton.artist.application.exception.NoSuchArtistException;
 import com.prography.lighton.artist.domain.entity.Artist;
 import com.prography.lighton.member.domain.entity.Member;
 import java.util.Optional;
@@ -10,4 +11,9 @@ import org.springframework.stereotype.Repository;
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
     Optional<Artist> findByMember(Member member);
+
+    default Artist getByMember(Member member) {
+        return findByMember(member)
+                .orElseThrow(NoSuchArtistException::new);
+    }
 }
