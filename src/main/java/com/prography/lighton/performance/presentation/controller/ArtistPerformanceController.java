060fc5f3102b472352814094b4b50092bcc1fa0e
yooookm <yookyungmi02@gmail.com>
2025-05-19 05:58:09 +0900

    feat: 공연 취소 api http method delete로 변경

--- src/main/java/com/prography/lighton/performance/presentation/controller/ArtistPerformanceController.java
+++ src/main/java/com/prography/lighton/performance/presentation/controller/ArtistPerformanceController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,7 +42,7 @@ public class ArtistPerformanceController {
                 .body(ApiUtils.success());
     }
 
-    @PostMapping("/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<ApiResult<String>> cancelPerformance(@PathVariable Long id) {
         // 나중에 수정 필요
         Member member = Member.withId(1L);
