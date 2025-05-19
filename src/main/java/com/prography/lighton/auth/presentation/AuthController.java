b0184065287b61a9aee791e5f8851e098e7db71e
msk226 <kminseok5167@gmail.com>
2025-05-19 14:58:57 +0900

    feat: 소셜 로그인 API 엔드포인트 작성

--- src/main/java/com/prography/lighton/auth/presentation/AuthController.java
+++ src/main/java/com/prography/lighton/auth/presentation/AuthController.java
@@ -2,10 +2,15 @@ package com.prography.lighton.auth.presentation;
 
 import com.prography.lighton.auth.application.OAuthUseCase;
 import com.prography.lighton.auth.domain.enums.SocialLoginType;
-import com.prography.lighton.auth.presentation.dto.response.RegisterOAuthMemberResponseDTO;
+import com.prography.lighton.common.utils.ApiUtils;
+import com.prography.lighton.common.utils.ApiUtils.ApiResult;
+import com.prography.lighton.member.presentation.dto.response.LoginMemberResponseDTO;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,18 +26,21 @@ public class AuthController {
     private final OAuthUseCase oAuthUseCase;
 
     @GetMapping("/oauth/{socialLoginType}")
-    public void socialLoginRedirect(@PathVariable(name = "socialLoginType") String SocialLoginPath) throws IOException {
-        SocialLoginType socialLoginType = SocialLoginType.valueOf(SocialLoginPath.toUpperCase());
-        oAuthUseCase.accessRequest(socialLoginType);
+    public void socialLoginRedirect(
+            @PathVariable(name = "socialLoginType") String socialLoginPath,
+            HttpServletResponse response) throws IOException {
+        SocialLoginType socialLoginType = SocialLoginType.valueOf(socialLoginPath.toUpperCase());
+        String redirectUrl = oAuthUseCase.accessRequest(socialLoginType);
+        response.sendRedirect(redirectUrl);
     }
 
+
     @GetMapping(value = "/oauth/{socialLoginType}/callback")
-    public RegisterOAuthMemberResponseDTO socialLoginCallback(
+    public ResponseEntity<ApiResult<LoginMemberResponseDTO>> socialLoginCallback(
             @PathVariable(name = "socialLoginType") String socialLoginPath,
-            @RequestParam(name = "code") String code) throws IOException, RuntimeException {
-        log.info(">> 소셜 로그인 API 서버로부터 받은 code : {}", code);
+            @RequestParam(name = "code") String code) throws RuntimeException {
         SocialLoginType socialLoginType = SocialLoginType.valueOf(socialLoginPath.toUpperCase());
-        oAuthUseCase.oAuthLoginOrJoin(socialLoginType, code);
-        return null;
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiUtils.success(oAuthUseCase.oAuthLoginOrJoin(socialLoginType, code)));
     }
 }
