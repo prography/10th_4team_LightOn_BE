195b0319fa5711e3a5def0a31b3d2d27b67e5c67
msk226 <kminseok5167@gmail.com>
2025-05-19 14:56:37 +0900

    feat: 로그인 공통 응답 DTO 작성

--- src/main/java/com/prography/lighton/member/presentation/dto/response/LoginMemberResponseDTO.java
+++ src/main/java/com/prography/lighton/member/presentation/dto/response/LoginMemberResponseDTO.java
@@ -1,13 +1,17 @@
 package com.prography.lighton.member.presentation.dto.response;
 
-public record LoginMemberResponseDTO (String accessToken,
-									  String refreshToken,
-									  Long memberId) {
-	public static LoginMemberResponseDTO of(
-			String accessToken,
-			String refreshToken,
-			Long memberId
-	) {
-		return new LoginMemberResponseDTO(accessToken, refreshToken, memberId);
-	}
+public record LoginMemberResponseDTO(
+        boolean isRegistered,
+        String accessToken,
+        String refreshToken,
+        Long memberId,
+        Long temporaryMemberId
+) {
+    public static LoginMemberResponseDTO registered(String accessToken, String refreshToken, Long memberId) {
+        return new LoginMemberResponseDTO(true, accessToken, refreshToken, memberId, null);
+    }
+
+    public static LoginMemberResponseDTO temporary(Long temporaryMemberId) {
+        return new LoginMemberResponseDTO(false, null, null, null, temporaryMemberId);
+    }
 }
