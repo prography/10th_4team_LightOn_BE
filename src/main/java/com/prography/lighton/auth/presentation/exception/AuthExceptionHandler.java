7ba2a64074d1d91fb3230fbac013766e446117a6
msk226 <kminseok5167@gmail.com>
2025-05-19 14:58:12 +0900

    feat: auth 예외 핸들러 및 커스텀 에러 제작

---
+++ src/main/java/com/prography/lighton/auth/presentation/exception/AuthExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.prography.lighton.auth.presentation.exception;
+
+import com.prography.lighton.auth.exception.MemberProfileIncompleteException;
+import com.prography.lighton.common.utils.ApiUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AuthExceptionHandler {
+
+    @ExceptionHandler(MemberProfileIncompleteException.class)
+    public ResponseEntity<?> handleMemberProfileIncompleteException(MemberProfileIncompleteException e) {
+        return ResponseEntity.status(e.status()).body(ApiUtils.error(e.status(), e.getMessage()));
+    }
+}
