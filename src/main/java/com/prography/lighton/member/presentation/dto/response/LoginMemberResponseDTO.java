740a541d63ce940c3027957a5646a23915f3f9cd
msk226 <kminseok5167@gmail.com>
2025-05-20 15:04:02 +0900

    feat: 로그인 성공 DTO에서 회원 ID 제외

--- src/main/java/com/prography/lighton/member/presentation/dto/response/LoginMemberResponseDTO.java
+++ src/main/java/com/prography/lighton/member/presentation/dto/response/LoginMemberResponseDTO.java
@@ -1,13 +1,11 @@
 package com.prography.lighton.member.presentation.dto.response;
 
-public record LoginMemberResponseDTO (String accessToken,
-									  String refreshToken,
-									  Long memberId) {
-	public static LoginMemberResponseDTO of(
-			String accessToken,
-			String refreshToken,
-			Long memberId
-	) {
-		return new LoginMemberResponseDTO(accessToken, refreshToken, memberId);
-	}
+public record LoginMemberResponseDTO(String accessToken,
+                                     String refreshToken) {
+    public static LoginMemberResponseDTO of(
+            String accessToken,
+            String refreshToken
+    ) {
+        return new LoginMemberResponseDTO(accessToken, refreshToken);
+    }
 }
