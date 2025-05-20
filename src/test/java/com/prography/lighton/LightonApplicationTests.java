c4b078704166fc91f4b391a8a7d5b30b698d76f2
msk226 <kminseok5167@gmail.com>
2025-05-20 23:40:14 +0900

    feat: 캐시 관련 클래스 MockBean 설정

--- src/test/java/com/prography/lighton/LightonApplicationTests.java
+++ src/test/java/com/prography/lighton/LightonApplicationTests.java
@@ -1,13 +1,23 @@
 package com.prography.lighton;
 
+import com.prography.lighton.genre.infrastructure.cache.GenreCache;
+import com.prography.lighton.region.infrastructure.cache.RegionCache;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class LightonApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @MockBean
+    private RegionCache regionCache;
+
+    @MockBean
+    private GenreCache genreCache;
+
 
 }
