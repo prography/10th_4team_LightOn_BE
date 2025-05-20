ce4ee3c3be8312cd35bcb518b78298463ba2aead
yooookm <yookyungmi02@gmail.com>
2025-05-20 13:51:45 +0900

    refactor: 캐시 초기화 실행 중 에러 발생 시 핸들링 로직 추가

--- src/main/java/com/prography/lighton/genre/infrastructure/cache/GenreCache.java
+++ src/main/java/com/prography/lighton/genre/infrastructure/cache/GenreCache.java
@@ -10,8 +10,10 @@ import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class GenreCache {
@@ -24,6 +26,12 @@ public class GenreCache {
     public void init() {
         this.cache = genreRepository.findAll().stream()
                 .collect(Collectors.toUnmodifiableMap(Genre::getId, Function.identity()));
+
+        if (cache.isEmpty()) {
+            throw new IllegalStateException("GenreCache 초기화 실패: 캐시 비어 있음");
+        }
+
+        log.info("장르 엔티티 캐시 초기화 성공 - size: {}", cache.size());
     }
 
     public List<Genre> getGenresOrThrow(List<Long> genreIds) {
--- src/main/java/com/prography/lighton/region/infrastructure/cache/RegionCache.java
+++ src/main/java/com/prography/lighton/region/infrastructure/cache/RegionCache.java
@@ -7,8 +7,10 @@ import jakarta.annotation.PostConstruct;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class RegionCache {
@@ -21,6 +23,12 @@ public class RegionCache {
                 .forEach(sr ->
                         cache.put(sr.getCode(), RegionInfo.of(sr.getRegion(), sr))
                 );
+
+        if (cache.isEmpty()) {
+            throw new IllegalStateException("RegionCache 초기화 실패: 캐시 비어 있음");
+        }
+
+        log.info("지역 엔티티 캐시 초기화 성공 - size: {}", cache.size());
     }
 
     public RegionInfo getRegionInfoByCode(Integer regionCode) {
