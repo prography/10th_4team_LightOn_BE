281659ab0d883cc3fce7820da87963febf6bc711
yooookm <yookyungmi02@gmail.com>
2025-05-20 15:46:01 +0900

    feat: artist 도메인 유효성 추가

--- src/main/java/com/prography/lighton/artist/domain/entity/vo/ActivityImages.java
+++ src/main/java/com/prography/lighton/artist/domain/entity/vo/ActivityImages.java
@@ -1,14 +1,13 @@
 package com.prography.lighton.artist.domain.entity.vo;
 
+import com.prography.lighton.common.domain.DomainValidator;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.util.List;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Embeddable
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ActivityImages {
     @Column(nullable = false)
@@ -21,6 +20,14 @@ public class ActivityImages {
     private String imageUrl4;
 
     private String imageUrl5;
+
+    public ActivityImages(String imageUrl1, String imageUrl2, String imageUrl3, String imageUrl4, String imageUrl5) {
+        this.imageUrl1 = DomainValidator.requireNonBlank(imageUrl1);
+        this.imageUrl2 = imageUrl2;
+        this.imageUrl3 = imageUrl3;
+        this.imageUrl4 = imageUrl4;
+        this.imageUrl5 = imageUrl5;
+    }
 
     public static ActivityImages of(List<String> urls) {
         String[] images = new String[5];
