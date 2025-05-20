db944ca05701b70c47d181549463ff2b64b3c599
yooookm <yookyungmi02@gmail.com>
2025-05-20 15:41:47 +0900

    feat: 빈 문자열 검증 함수 구현 - 로그 남기기

--- src/main/java/com/prography/lighton/common/domain/DomainValidator.java
+++ src/main/java/com/prography/lighton/common/domain/DomainValidator.java
@@ -1,14 +1,14 @@
 package com.prography.lighton.common.domain;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 
+@Slf4j
 public class DomainValidator {
 
-    public static String requireNonBlank(String value, Class<?> entityClass, String fieldName) {
+    public static String requireNonBlank(String value) {
         if (!StringUtils.hasText(value)) {
-            throw new IllegalArgumentException(
-                    String.format("[%s] %s 는 빈 문자열을 넣을 수 없습니다.", entityClass.getSimpleName(), fieldName)
-            );
+            throw new IllegalArgumentException("해당 필드는 빈 문자열을 넣을 수 없습니다.");
         }
         return value;
     }
