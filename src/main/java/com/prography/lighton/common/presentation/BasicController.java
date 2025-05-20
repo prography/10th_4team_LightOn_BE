5d5f219f84ef0502ffd73d67d0d266b2c61e3f04
yooookm <yookyungmi02@gmail.com>
2025-05-20 15:11:00 +0900

    refactor: common controller 패키지 이름 presentation으로 변경

--- src/main/java/com/prography/lighton/common/presentation/BasicController.java
+++ src/main/java/com/prography/lighton/common/presentation/BasicController.java
@@ -1,14 +1,13 @@
-package com.prography.lighton.common.controller;
+package com.prography.lighton.common.presentation;
 
 import com.prography.lighton.common.utils.ApiUtils;
 import com.prography.lighton.common.utils.ApiUtils.ApiResult;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 public class BasicController {
+
     @GetMapping("/health")
     public ApiResult<String> healthCheck() {
         return ApiUtils.success("ok");
