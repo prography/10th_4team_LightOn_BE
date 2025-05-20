3dd6b789199adc72110c64a91a5914d49b7ba06f
msk226 <kminseok5167@gmail.com>
2025-05-21 08:57:56 +0900

    feat: 레포지토리 infrastructure 계층으로 이동

---
+++ src/main/java/com/prography/lighton/member/infrastructure/repository/MemberRepository.java
@@ -0,0 +1,23 @@
+package com.prography.lighton.member.infrastructure.repository;
+
+import com.prography.lighton.member.domain.entity.Member;
+import com.prography.lighton.member.domain.entity.vo.Email;
+import com.prography.lighton.member.domain.entity.vo.Phone;
+import com.prography.lighton.member.exception.NoSuchMemberException;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(Email email);
+
+    boolean existsByPhone(Phone phone);
+
+    default Member getMemberByEmail(Email email) {
+        return findByEmail(email)
+                .orElseThrow(NoSuchMemberException::new);
+    }
+
+}
