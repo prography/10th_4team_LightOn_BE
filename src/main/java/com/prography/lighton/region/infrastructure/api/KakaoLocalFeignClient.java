c80f49d66a06943b3c4d5353ebd1dc632b2e4501
yooookm <yookyungmi02@gmail.com>
2025-05-22 18:50:55 +0900

    feat: 카카오맵 api 호출 기능 구현

---
+++ src/main/java/com/prography/lighton/region/infrastructure/api/KakaoLocalFeignClient.java
@@ -0,0 +1,16 @@
+package com.prography.lighton.region.infrastructure.api;
+
+import com.prography.lighton.region.application.dto.KakaoAddressSearchResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(
+        name = "kakaoLocal",
+        url = "${kakao.local.base-url}"
+)
+public interface KakaoLocalFeignClient {
+
+    @GetMapping("/v2/local/search/address.json")
+    KakaoAddressSearchResponse search(@RequestParam("query") String address);
+}
