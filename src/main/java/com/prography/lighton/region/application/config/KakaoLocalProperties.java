fd98a539608301fb617fee4f2339d4f91b0688eb
yooookm <yookyungmi02@gmail.com>
2025-05-22 18:58:28 +0900

    feat: 카카오 설정 정보 객체 제거

--- src/main/java/com/prography/lighton/region/application/config/KakaoLocalProperties.java
+++
@@ -1,9 +0,0 @@
-package com.prography.lighton.region.application.config;
-
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("kakao.local")
-public record KakaoLocalProperties(String baseUrl, String restApiKey) {
-
-}
--- src/main/java/com/prography/lighton/region/infrastructure/api/KakaoAuthRequestInterceptor.java
+++ src/main/java/com/prography/lighton/region/infrastructure/api/KakaoAuthRequestInterceptor.java
@@ -1,20 +1,21 @@
 package com.prography.lighton.region.infrastructure.api;
 
-import com.prography.lighton.region.application.config.KakaoLocalProperties;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class KakaoAuthRequestInterceptor implements RequestInterceptor {
 
-    private final KakaoLocalProperties props;
-
+    @Value("${kakao.local.rest-api-key}")
+    private String restApiKey;
+    
     @Override
     public void apply(RequestTemplate template) {
-        template.header("Authorization", "KakaoAK " + props.restApiKey());
+        template.header("Authorization", "KakaoAK " + restApiKey);
         template.header("Content-Type", "application/json");
     }
 }
