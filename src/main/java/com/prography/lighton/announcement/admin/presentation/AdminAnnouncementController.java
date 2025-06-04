fff5e5961912d217b453da76447bd464aacdfaa8
msk226 <kminseok5167@gmail.com>
2025-06-04 15:45:33 +0900

    feat: 공지사항 생성/수정/삭제 API에 관리자 애노테이션 및 서비스 로직 연결

--- src/main/java/com/prography/lighton/announcement/admin/presentation/AdminAnnouncementController.java
+++ src/main/java/com/prography/lighton/announcement/admin/presentation/AdminAnnouncementController.java
@@ -1,6 +1,8 @@
 package com.prography.lighton.announcement.admin.presentation;
 
+import com.prography.lighton.announcement.admin.application.ManageAnnouncementUseCase;
 import com.prography.lighton.announcement.admin.presentation.dto.request.ManageAnnouncementRequestDTO;
+import com.prography.lighton.common.annotation.AdminOnly;
 import com.prography.lighton.common.utils.ApiUtils;
 import com.prography.lighton.common.utils.ApiUtils.ApiResult;
 import jakarta.validation.Valid;
@@ -15,26 +17,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/admin/announcements/")
+@RequestMapping("/api/admin/announcements")
 @RequiredArgsConstructor
 public class AdminAnnouncementController {
 
+    private final ManageAnnouncementUseCase manageAnnouncementUseCase;
+
     @PostMapping()
+    @AdminOnly
     public ResponseEntity<ApiResult<?>> registerAnnouncement(
             @RequestBody @Valid ManageAnnouncementRequestDTO request) {
+        manageAnnouncementUseCase.registerAnnouncement(request);
         return ResponseEntity.ok(ApiUtils.success());
     }
 
     @PutMapping("/{announcementId}")
+    @AdminOnly
     public ResponseEntity<ApiResult<?>> registerAnnouncement(
             @RequestBody @Valid ManageAnnouncementRequestDTO request,
             @PathVariable Long announcementId) {
+        manageAnnouncementUseCase.updateAnnouncement(announcementId, request);
         return ResponseEntity.ok(ApiUtils.success());
     }
 
     @DeleteMapping("/{announcementId}")
+    @AdminOnly
     public ResponseEntity<ApiResult<?>> deleteAnnouncement(
             @PathVariable Long announcementId) {
+        manageAnnouncementUseCase.deleteAnnouncement(announcementId);
         return ResponseEntity.ok(ApiUtils.success());
     }
 
