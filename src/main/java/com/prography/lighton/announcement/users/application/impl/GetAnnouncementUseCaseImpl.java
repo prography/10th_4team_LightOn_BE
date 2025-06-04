e65d9c3f6b4baaf5b9918aea18dc15e9fd9213a9
msk226 <kminseok5167@gmail.com>
2025-06-04 15:57:01 +0900

    feat: 공지사항 조회 내부 로직 작성

---
+++ src/main/java/com/prography/lighton/announcement/users/application/impl/GetAnnouncementUseCaseImpl.java
@@ -0,0 +1,40 @@
+package com.prography.lighton.announcement.users.application.impl;
+
+import static org.springframework.data.domain.PageRequest.of;
+
+import com.prography.lighton.announcement.common.domain.entity.Announcement;
+import com.prography.lighton.announcement.users.application.GetAnnouncementUseCase;
+import com.prography.lighton.announcement.users.infrastructure.UserAnnouncementRepository;
+import com.prography.lighton.announcement.users.presentation.dto.response.GetAnnouncementDetailResponseDTO;
+import com.prography.lighton.announcement.users.presentation.dto.response.GetAnnouncementListResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class GetAnnouncementUseCaseImpl implements GetAnnouncementUseCase {
+
+    private final UserAnnouncementRepository userAnnouncementRepository;
+
+    @Override
+    public GetAnnouncementDetailResponseDTO getAnnouncementDetail(Long announcementId) {
+        Announcement announcement = userAnnouncementRepository.getById(announcementId);
+
+        return GetAnnouncementDetailResponseDTO.of(
+                announcement.getId(),
+                announcement.getTitle(),
+                announcement.getContent(),
+                announcement.getImageUrl());
+    }
+
+    @Override
+    public GetAnnouncementListResponseDTO getAnnouncementList(int page, int size) {
+        Page<Announcement> announcements = userAnnouncementRepository.findAllByOrderByCreatedAtDesc(
+                of(page, size));
+
+        return GetAnnouncementListResponseDTO.of(announcements);
+    }
+}
