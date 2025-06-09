a441c5e69712e55d15f956ebaae942cd9584eb93
msk226 <kminseok5167@gmail.com>
2025-06-09 19:22:29 +0900

    feat: 누락된 테스트 코드 의존성 추가

--- src/test/java/com/prography/lighton/common/presentation/ControllerTest.java
+++ src/test/java/com/prography/lighton/common/presentation/ControllerTest.java
@@ -1,11 +1,15 @@
 package com.prography.lighton.common.presentation;
 
 import com.prography.lighton.auth.application.TokenProvider;
+import com.prography.lighton.genre.infrastructure.cache.GenreCache;
 import com.prography.lighton.member.application.CompleteMemberProfileUseCase;
 import com.prography.lighton.member.application.LoginMemberUseCase;
+import com.prography.lighton.member.application.ManagePreferredGenreUseCase;
 import com.prography.lighton.member.application.RegisterMemberUseCase;
 import com.prography.lighton.member.infrastructure.repository.MemberRepository;
+import com.prography.lighton.member.infrastructure.repository.PreferredGenreRepository;
 import com.prography.lighton.member.presentation.MemberController;
+import com.prography.lighton.region.infrastructure.cache.RegionCache;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -29,9 +33,20 @@ public abstract class ControllerTest {
     protected LoginMemberUseCase loginMemberUseCase;
 
     @MockBean
+    protected ManagePreferredGenreUseCase managePreferredGenreUseCase;
+
+    @MockBean
     protected TokenProvider tokenProvider;
 
     @MockBean
     protected MemberRepository memberRepository;
+
+    @MockBean
+    protected PreferredGenreRepository preferredGenreRepository;
+
+    @MockBean
+    protected GenreCache genreCache;
+    @MockBean
+    protected RegionCache regionCache;
 
 }
