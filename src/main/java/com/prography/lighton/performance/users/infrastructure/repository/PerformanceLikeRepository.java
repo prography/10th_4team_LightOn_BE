5eca20330456915250226352a70e18d1f2c1cdb8
yooookm <yookyungmi02@gmail.com>
2025-06-15 23:35:58 +0900

    feat: member, performance로 공연 좋아요 엔티티 조회

---
+++ src/main/java/com/prography/lighton/performance/users/infrastructure/repository/PerformanceLikeRepository.java
@@ -0,0 +1,14 @@
+package com.prography.lighton.performance.users.infrastructure.repository;
+
+import com.prography.lighton.member.common.domain.entity.Member;
+import com.prography.lighton.performance.common.domain.entity.Performance;
+import com.prography.lighton.performance.common.domain.entity.PerformanceLike;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PerformanceLikeRepository extends JpaRepository<PerformanceLike, Long> {
+
+    Optional<PerformanceLike> findByMemberAndPerformance(Member member, Performance performance);
+
+}
+
