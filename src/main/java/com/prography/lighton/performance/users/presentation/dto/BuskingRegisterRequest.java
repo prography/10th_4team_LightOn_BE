bb79e411d71e14139ae4fb49e9ac61189a3142ab
yooookm <yookyungmi02@gmail.com>
2025-06-15 20:30:39 +0900

    feat: 버스킹 생성/수정 request 작성

---
+++ src/main/java/com/prography/lighton/performance/users/presentation/dto/BuskingRegisterRequest.java
@@ -0,0 +1,24 @@
+package com.prography.lighton.performance.users.presentation.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.URL;
+
+public record BuskingRegisterRequest(
+
+        @NotNull(message = "공연 기본 정보는 필수입니다.")
+        @Valid
+        InfoDTO info,
+
+        @NotNull(message = "공연 일정 정보는 필수입니다.")
+        @Valid
+        ScheduleDTO schedule,
+
+        @NotBlank(message = "공연 증빙 자료 URL은 필수입니다.")
+        @URL(message = "공연 증빙 자료는 올바른 URL 형식이어야 합니다.")
+        String proof
+
+) {
+}
+
