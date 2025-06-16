93c9ec8dee8f905fe69d322b984bc62f43cf4468
yooookm <yookyungmi02@gmail.com>
2025-06-16 22:35:51 +0900

    feat: 아티스트/사용자별 버스킹 생성 함수 분리

--- src/main/java/com/prography/lighton/performance/common/domain/entity/Busking.java
+++ src/main/java/com/prography/lighton/performance/common/domain/entity/Busking.java
@@ -1,19 +1,24 @@
 package com.prography.lighton.performance.common.domain.entity;
 
+import static lombok.AccessLevel.PRIVATE;
 import static lombok.AccessLevel.PROTECTED;
 
+import com.prography.lighton.artist.common.domain.entity.Artist;
 import com.prography.lighton.common.domain.DomainValidator;
 import com.prography.lighton.genre.domain.entity.Genre;
 import com.prography.lighton.member.common.domain.entity.Member;
+import com.prography.lighton.performance.common.domain.entity.enums.ApproveStatus;
 import com.prography.lighton.performance.common.domain.entity.enums.Seat;
 import com.prography.lighton.performance.common.domain.entity.enums.Type;
 import com.prography.lighton.performance.common.domain.entity.vo.Info;
 import com.prography.lighton.performance.common.domain.entity.vo.Location;
 import com.prography.lighton.performance.common.domain.entity.vo.Payment;
 import com.prography.lighton.performance.common.domain.entity.vo.Schedule;
+import jakarta.persistence.Column;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -21,19 +26,33 @@ import lombok.NoArgsConstructor;
 @DiscriminatorValue("BUSKING")
 @Getter
 @NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PRIVATE)
 public class Busking extends Performance {
 
     private static final int UPDATE_DEADLINE_DAYS = 3;
 
-    public static Busking create(
+    @Column(length = 100)
+    private String artistName;
+
+    @Column(length = 255)
+    private
+    String artistDescription;
+
+    public static Busking createByUser(
             Member performer,
             Info info,
             Schedule schedule,
             Location location,
             List<Genre> genres,
-            String proofUrl
+            String proofUrl,
+            String artistName,
+            String artistDescription
     ) {
-        Busking busking = new Busking();
+        DomainValidator.requireNonBlank(artistName);
+        DomainValidator.requireNonBlank(artistDescription);
+        DomainValidator.requireNonBlank(proofUrl);
+
+        Busking busking = new Busking(artistName, artistDescription);
         busking.initCommonFields(
                 performer,
                 info,
@@ -45,6 +64,33 @@ public class Busking extends Performance {
                 proofUrl,
                 genres
         );
+        return busking;
+    }
+
+    public static Busking createByArtist(
+            Member performer,
+            Info info,
+            Schedule schedule,
+            Location location,
+            List<Genre> genres,
+            String proofUrl,
+            Artist artist
+    ) {
+        DomainValidator.requireNonBlank(proofUrl);
+
+        Busking busking = new Busking(artist.getStageName(), artist.getDescription());
+        busking.initCommonFields(
+                performer,
+                info,
+                schedule,
+                location,
+                Payment.free(),
+                Type.BUSKING,
+                Seat.STANDING,
+                proofUrl,
+                genres
+        );
+        busking.managePerformanceApplication(ApproveStatus.APPROVED);
         return busking;
     }
 
