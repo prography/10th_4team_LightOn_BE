6510c037b3100f0b2de9f1362b7db26e301e72e1
msk226 <kminseok5167@gmail.com>
2025-06-27 14:16:35 +0900

    feature: 공연 상세 조회 API 시그니처 작성

--- src/main/java/com/prography/lighton/performance/users/presentation/controller/UserPerformanceController.java
+++ src/main/java/com/prography/lighton/performance/users/presentation/controller/UserPerformanceController.java
@@ -5,9 +5,11 @@ import com.prography.lighton.common.utils.ApiUtils;
 import com.prography.lighton.common.utils.ApiUtils.ApiResult;
 import com.prography.lighton.member.common.domain.entity.Member;
 import com.prography.lighton.performance.users.application.service.UserPerformanceService;
+import com.prography.lighton.performance.users.presentation.dto.response.GetPerformanceDetailResponseDTO;
 import com.prography.lighton.performance.users.presentation.dto.response.RequestPerformanceResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserPerformanceController {
 
     private final UserPerformanceService userPerformanceService;
+
+    @GetMapping("/{performanceId}")
+    public ResponseEntity<ApiResult<GetPerformanceDetailResponseDTO>> getPerformanceDetail(
+            @PathVariable Long performanceId) {
+        return ResponseEntity.ok(ApiUtils.success(null));
+    }
 
     @PostMapping("/{performanceId}")
     public ResponseEntity<ApiResult<RequestPerformanceResponseDTO>> requestForPerformance(
---
+++ src/main/java/com/prography/lighton/performance/users/presentation/dto/response/GetPerformanceDetailResponseDTO.java
@@ -0,0 +1,55 @@
+package com.prography.lighton.performance.users.presentation.dto.response;
+
+import com.prography.lighton.performance.common.domain.entity.enums.Seat;
+import com.prography.lighton.performance.common.domain.entity.enums.Type;
+import com.prography.lighton.performance.common.domain.entity.vo.Info;
+import com.prography.lighton.performance.common.domain.entity.vo.Schedule;
+import java.util.List;
+
+public record GetPerformanceDetailResponseDTO(
+        Long id,
+        Info info,
+        List<PerformanceArtistDTO> artists,
+        List<String> genres,
+        Schedule schedule,
+        Integer regionCode,
+        String regionName,
+        Type type,
+        List<Seat> seats,
+        Integer totalSeatsCount,
+        Integer bookedSeatCount,
+        String proofUrl
+
+) {
+    public static GetPerformanceDetailResponseDTO of(
+            Long id,
+            Info info,
+            List<PerformanceArtistDTO> artists,
+            List<String> genres,
+            Schedule schedule,
+            Integer regionCode,
+            String regionName,
+            Type type,
+            List<Seat> seats,
+            Integer totalSeatsCount,
+            Integer bookedSeatCount,
+            String proofUrl
+    ) {
+        return new GetPerformanceDetailResponseDTO(id, info, artists, genres, schedule, regionCode,
+                regionName, type,
+                seats,
+                totalSeatsCount,
+                bookedSeatCount,
+                proofUrl);
+    }
+
+    public record PerformanceArtistDTO(
+            Long id,
+            String name,
+            String description
+    ) {
+        public static PerformanceArtistDTO of(Long id, String name, String description) {
+            return new PerformanceArtistDTO(id, name, description);
+        }
+    }
+}
