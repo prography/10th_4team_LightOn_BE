133c125e97737f539573e25839b077ac600e30a0
yooookm <yookyungmi02@gmail.com>
2025-06-28 16:38:58 +0900

    feat: GetPerformanceApplicationDetailResponseDTO에서 조립 로직 삭제 - 빌더 추가

--- src/main/java/com/prography/lighton/performance/admin/presentation/dto/response/GetPerformanceApplicationDetailResponseDTO.java
+++ src/main/java/com/prography/lighton/performance/admin/presentation/dto/response/GetPerformanceApplicationDetailResponseDTO.java
@@ -5,11 +5,13 @@ import com.prography.lighton.performance.common.domain.entity.enums.Type;
 import com.prography.lighton.performance.common.domain.entity.vo.Info;
 import com.prography.lighton.performance.common.domain.entity.vo.Schedule;
 import java.util.List;
+import lombok.Builder;
 
+@Builder
 public record GetPerformanceApplicationDetailResponseDTO(
         Long id,
         Info info,
-        List<PerformanceArtistDTO> artists,
+        List<ArtistDTO> artists,
         List<String> genres,
         Schedule schedule,
         Integer regionCode,
@@ -17,33 +19,7 @@ public record GetPerformanceApplicationDetailResponseDTO(
         Type type,
         List<Seat> seats,
         String proofUrl
-
 ) {
-    public static GetPerformanceApplicationDetailResponseDTO of(
-            Long id,
-            Info info,
-            List<PerformanceArtistDTO> artists,
-            List<String> genres,
-            Schedule schedule,
-            Integer regionCode,
-            String regionName,
-            Type type,
-            List<Seat> seats,
-            String proofUrl
-    ) {
-        return new GetPerformanceApplicationDetailResponseDTO(id, info, artists, genres, schedule, regionCode,
-                regionName, type,
-                seats,
-                proofUrl);
-    }
-
-    public record PerformanceArtistDTO(
-            Long id,
-            String name,
-            String description
-    ) {
-        public static PerformanceArtistDTO of(Long id, String name, String description) {
-            return new PerformanceArtistDTO(id, name, description);
-        }
+    public record ArtistDTO(Long id, String name, String description) {
     }
 }
