c6e1aa9c7991a9bf9f052ec4da441fb2341a413d
yooookm <yookyungmi02@gmail.com>
2025-06-28 23:46:59 +0900

    feat: db에서 공연 summary 조회 기능 구현

--- src/main/java/com/prography/lighton/performance/users/infrastructure/repository/PerformanceRecommendationRepositoryImpl.java
+++ src/main/java/com/prography/lighton/performance/users/infrastructure/repository/PerformanceRecommendationRepositoryImpl.java
@@ -1,9 +1,14 @@
 package com.prography.lighton.performance.users.infrastructure.repository;
 
+import static com.querydsl.core.group.GroupBy.groupBy;
+import static com.querydsl.core.group.GroupBy.list;
+import static com.querydsl.core.types.Projections.constructor;
+
 import com.prography.lighton.member.common.domain.entity.association.QPreferredGenre;
 import com.prography.lighton.performance.common.domain.entity.QPerformance;
 import com.prography.lighton.performance.common.domain.entity.association.QPerformanceGenre;
 import com.prography.lighton.performance.common.domain.entity.enums.ApproveStatus;
+import com.prography.lighton.performance.users.infrastructure.dto.PerformanceSummary;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.LocalDate;
 import java.util.List;
@@ -42,4 +47,33 @@ public class PerformanceRecommendationRepositoryImpl
                 .fetch();
     }
 
+    @Override
+    public List<PerformanceSummary> findSummaries(List<Long> ids) {
+        if (ids.isEmpty()) {
+            return List.of();
+        }
+
+        QPerformance p = QPerformance.performance;
+        QPerformanceGenre pg = QPerformanceGenre.performanceGenre;
+
+        return query
+                .from(p)
+                .leftJoin(p.genres, pg).fetchJoin()
+                .where(p.id.in(ids))
+                .transform(
+                        groupBy(p.id).list(
+                                constructor(PerformanceSummary.class,
+                                        p.id,
+                                        p.info.title,
+                                        p.info.description,
+                                        p.info.posterUrl,
+                                        p.schedule.startDate,
+                                        p.schedule.startTime,
+                                        p.payment.isPaid,
+                                        p.location.region.subRegion.name,
+                                        list(pg.genre.name)
+                                )
+                        )
+                );
+    }
 }
