7c573d48c125ccaa8f4fdb98729ddb5124472a28
yooookm <yookyungmi02@gmail.com>
2025-06-28 23:23:36 +0900

    feat: db에서 쓸 공연 summary dto 작성

---
+++ src/main/java/com/prography/lighton/performance/users/infrastructure/dto/PerformanceSummary.java
@@ -0,0 +1,18 @@
+package com.prography.lighton.performance.users.infrastructure.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+public record PerformanceSummary(
+        Long id,
+        String title,
+        String description,
+        String posterUrl,
+        LocalDate startDate,
+        LocalTime startTime,
+        Boolean isPaid,
+        Long regionCode,
+        List<String> genreNames
+) {
+}
