4cbf7e97d1c4456b518ebe10a2c3e5da1ddade3f
yooookm <yookyungmi02@gmail.com>
2025-07-03 23:40:08 +0900

    refactor: 인기공연 조회 repository interface 구조 변경 - 장르 유무에 따른 함수 분리 - 장르 매개변수에 따라 함수 호출 분리

--- src/main/java/com/prography/lighton/performance/users/infrastructure/repository/PerformancePopularRepository.java
+++ src/main/java/com/prography/lighton/performance/users/infrastructure/repository/PerformancePopularRepository.java
@@ -1,8 +1,18 @@
 package com.prography.lighton.performance.users.infrastructure.repository;
 
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public interface PerformancePopularRepository {
 
-    List<Long> findTopPopularIds(String genre, int limit);
+    List<Long> findTopPopularAll(int limit);
+
+    List<Long> findTopPopularByGenre(String genre, int limit);
+
+    default List<Long> findTopPopularIds(String genre, int limit) {
+        if (StringUtils.isBlank(genre)) {
+            return findTopPopularAll(limit);
+        }
+        return findTopPopularByGenre(genre.trim().toLowerCase(), limit);
+    }
 }
