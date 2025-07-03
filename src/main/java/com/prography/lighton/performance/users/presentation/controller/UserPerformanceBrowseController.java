a35b806f6ee4eb931c92a3cf5be695452fc8690f
yooookm <yookyungmi02@gmail.com>
2025-07-04 00:51:18 +0900

    fix: 컨트롤러 어노테이션 변경

--- src/main/java/com/prography/lighton/performance/users/presentation/controller/UserPerformanceBrowseController.java
+++ src/main/java/com/prography/lighton/performance/users/presentation/controller/UserPerformanceBrowseController.java
@@ -9,12 +9,12 @@ import com.prography.lighton.performance.users.application.service.UserRecommend
 import com.prography.lighton.performance.users.presentation.dto.response.GetPerformanceBrowseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/members/performances")
 @RequiredArgsConstructor
 public class UserPerformanceBrowseController {
