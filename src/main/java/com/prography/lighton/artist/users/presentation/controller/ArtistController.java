e1c43dba4d97f8b5260959d5873d43113027f397
yooookm <yookyungmi02@gmail.com>
2025-07-15 22:18:35 +0900

    feat: 아티스트 등록 api에서 multifile request 추가

--- src/main/java/com/prography/lighton/artist/users/presentation/controller/ArtistController.java
+++ src/main/java/com/prography/lighton/artist/users/presentation/controller/ArtistController.java
@@ -1,6 +1,7 @@
 package com.prography.lighton.artist.users.presentation.controller;
 
 import com.prography.lighton.artist.users.application.service.ArtistService;
+import com.prography.lighton.artist.users.presentation.dto.request.RegisterArtistMultipart;
 import com.prography.lighton.artist.users.presentation.dto.request.RegisterArtistRequest;
 import com.prography.lighton.artist.users.presentation.dto.request.UpdateArtistRequest;
 import com.prography.lighton.artist.users.presentation.dto.response.ArtistCheckResponseDTO;
@@ -9,7 +10,11 @@ import com.prography.lighton.common.utils.ApiUtils;
 import com.prography.lighton.common.utils.ApiUtils.ApiResult;
 import com.prography.lighton.member.common.domain.entity.Member;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +22,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/artists")
@@ -27,8 +35,22 @@ public class ArtistController {
     private final ArtistService artistService;
 
     @PostMapping
-    public ResponseEntity<ApiResult<String>> registerArtist(@LoginMember Member member,
-                                                            @Valid @RequestBody RegisterArtistRequest request) {
+    public ResponseEntity<ApiResult<String>> registerArtist(
+            @LoginMember Member member,
+
+            @Valid @RequestPart("data")
+            RegisterArtistRequest data,
+
+            @NotNull @RequestPart("profileImage")
+            MultipartFile profileImage,
+
+            @NotNull @RequestPart("proof")
+            MultipartFile proof,
+
+            @Size(max = 5)
+            @RequestPart(value = "activityPhotos", required = false)
+            List<MultipartFile> activityPhotos) {
+        RegisterArtistMultipart request = new RegisterArtistMultipart(data, profileImage, proof, activityPhotos);
         artistService.registerArtist(member, request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiUtils.success());
