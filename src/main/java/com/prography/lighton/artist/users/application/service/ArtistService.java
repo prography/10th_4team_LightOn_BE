b26203a27353165fb3472afcae45b9de6a3dbfef
yooookm <yookyungmi02@gmail.com>
2025-07-16 17:15:25 +0900

    feat: artist 등록/수정 로직에 resolver 함수 적용

--- src/main/java/com/prography/lighton/artist/users/application/service/ArtistService.java
+++ src/main/java/com/prography/lighton/artist/users/application/service/ArtistService.java
@@ -5,17 +5,12 @@ import static java.util.stream.Collectors.toList;
 
 import com.prography.lighton.artist.common.domain.entity.Artist;
 import com.prography.lighton.artist.common.domain.entity.enums.ApproveStatus;
-import com.prography.lighton.artist.common.domain.entity.vo.History;
 import com.prography.lighton.artist.users.application.exception.NoSuchArtistException;
+import com.prography.lighton.artist.users.application.resolver.ArtistRequestResolver;
 import com.prography.lighton.artist.users.infrastructure.repository.ArtistRepository;
-import com.prography.lighton.artist.users.presentation.dto.request.ArtistDTO;
-import com.prography.lighton.artist.users.presentation.dto.request.HistoryDTO;
 import com.prography.lighton.artist.users.presentation.dto.request.RegisterArtistMultipart;
 import com.prography.lighton.artist.users.presentation.dto.request.UpdateArtistMultipart;
 import com.prography.lighton.artist.users.presentation.dto.response.ArtistCheckResponseDTO;
-import com.prography.lighton.common.application.s3.S3UploadService;
-import com.prography.lighton.common.domain.vo.RegionInfo;
-import com.prography.lighton.genre.domain.entity.Genre;
 import com.prography.lighton.genre.infrastructure.cache.GenreCache;
 import com.prography.lighton.member.common.domain.entity.Member;
 import com.prography.lighton.region.infrastructure.cache.RegionCache;
@@ -32,7 +27,7 @@ public class ArtistService {
     private final ArtistRepository artistRepository;
     private final GenreCache genreCache;
     private final RegionCache regionCache;
-    private final S3UploadService uploadService;
+    private final ArtistRequestResolver resolver;
 
     public Artist getApprovedArtistByMember(Member member) {
         Artist artist = artistRepository.getByMember(member);
@@ -56,18 +51,7 @@ public class ArtistService {
         artistRepository.findByMember(member)
                 .ifPresent(Artist::isValidRecreatable);
 
-        var data = toArtistData(request.data().artist(), request.data().history());
-        Artist artist = Artist.create(
-                member,
-                request.data().artist().name(),
-                request.data().artist().description(),
-                uploadService.uploadFile(request.profileImage(), member),
-                data.activityRegion(),
-                data.history(),
-                uploadService.uploadFile(request.proof(), member),
-                data.genres()
-        );
-
+        Artist artist = resolver.toNewEntity(member, request);
         artistRepository.save(artist);
     }
 
@@ -75,37 +59,17 @@ public class ArtistService {
     public void updateArtist(Member member, UpdateArtistMultipart request) {
         Artist artist = getApprovedArtistByMember(member);
 
-        var data = toArtistData(request.data().artist(), request.data().history());
-        artist.update(
-                request.data().artist().name(),
-                request.data().artist().description(),
-                //request.artist().profileImage(),
-                "임시.url",
+        var data = resolver.toUpdateEntity(artist, request);
+        artist.update(data.stageName(),
+                data.description(),
+                data.profileUrl(),
                 data.activityRegion(),
                 data.history(),
-                data.genres()
-        );
+                data.genres());
     }
 
     public ArtistCheckResponseDTO isArtist(Member member) {
         return ArtistCheckResponseDTO.of(
                 artistRepository.existsByMemberAndApproveStatus(member, ApproveStatus.APPROVED));
-    }
-
-    private ArtistData toArtistData(
-            ArtistDTO artistDto,
-            HistoryDTO historyDto
-    ) {
-        RegionInfo activityRegion = regionCache.getRegionInfoByCode(artistDto.activityLocation());
-        History history = History.of(historyDto.bio(), List.of("image"));
-        List<Genre> genres = genreCache.getGenresByNameOrThrow(artistDto.genre());
-        return new ArtistData(activityRegion, history, genres);
-    }
-
-    private static record ArtistData(
-            RegionInfo activityRegion,
-            History history,
-            List<Genre> genres
-    ) {
     }
 }
