c043e94bf7a67004804d4151ee564d5e09ced79a
yooookm <yookyungmi02@gmail.com>
2025-07-16 19:41:41 +0900

    chore: UserBuskingController 함수명에 버스킹 명시

--- src/main/java/com/prography/lighton/performance/users/presentation/controller/UserBuskingController.java
+++ src/main/java/com/prography/lighton/performance/users/presentation/controller/UserBuskingController.java
@@ -5,7 +5,7 @@ import com.prography.lighton.common.utils.ApiUtils;
 import com.prography.lighton.common.utils.ApiUtils.ApiResult;
 import com.prography.lighton.member.common.domain.entity.Member;
 import com.prography.lighton.performance.users.application.service.BuskingService;
-import com.prography.lighton.performance.users.presentation.dto.request.UserBuskingRegisterRequest;
+import com.prography.lighton.performance.users.presentation.dto.request.SaveUserBuskingRequest;
 import com.prography.lighton.performance.users.presentation.dto.request.UserBuskingUpdateRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,25 +27,25 @@ public class UserBuskingController {
     private final BuskingService buskingService;
 
     @PostMapping
-    public ResponseEntity<ApiResult<String>> registerPerformance(
+    public ResponseEntity<ApiResult<String>> registerBusking(
             @LoginMember Member member,
-            @Valid @RequestBody UserBuskingRegisterRequest request) {
+            @Valid @RequestBody SaveUserBuskingRequest request) {
         buskingService.registerBuskingByUser(member, request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ApiUtils.success());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResult<String>> updatePerformance(@LoginMember Member member,
-                                                               @PathVariable Long id,
-                                                               @Valid @RequestBody UserBuskingUpdateRequest request) {
+    public ResponseEntity<ApiResult<String>> updateBusking(@LoginMember Member member,
+                                                           @PathVariable Long id,
+                                                           @Valid @RequestBody UserBuskingUpdateRequest request) {
         buskingService.updateBuskingByUser(member, id, request);
         return ResponseEntity.ok()
                 .body(ApiUtils.success());
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResult<String>> cancelPerformance(@LoginMember Member member, @PathVariable Long id) {
+    public ResponseEntity<ApiResult<String>> cancelBusking(@LoginMember Member member, @PathVariable Long id) {
         buskingService.cancelBusking(member, id);
         return ResponseEntity.ok()
                 .body(ApiUtils.success());
