9f1804cdd09a2fdea39ffb57fd7e57849e40873c
msk226 <kminseok5167@gmail.com>
2025-07-21 15:14:12 +0900

    feature: 같은 위치에 광고를 등록하려고 하면 예외 발생

--- src/main/java/com/prography/lighton/advertisement/admin/application/AdminAdvertisementCommandService.java
+++ src/main/java/com/prography/lighton/advertisement/admin/application/AdminAdvertisementCommandService.java
@@ -1,8 +1,10 @@
 package com.prography.lighton.advertisement.admin.application;
 
+import com.prography.lighton.advertisement.admin.application.exception.DuplicateDisplayOrderException;
 import com.prography.lighton.advertisement.admin.infrastructure.AdminAdvertisementRepository;
 import com.prography.lighton.advertisement.admin.presentation.dto.request.SaveAdvertisementRequestDTO;
 import com.prography.lighton.advertisement.common.domain.entity.Advertisement;
+import com.prography.lighton.advertisement.common.domain.entity.enums.Position;
 import com.prography.lighton.common.application.s3.S3UploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,23 +18,31 @@ public class AdminAdvertisementCommandService {
 
     private static final String IMAGE_PREFIX = "ads";
 
-    private final AdminAdvertisementRepository advertisementRepository;
+    private final AdminAdvertisementRepository adminAdvertisementRepository;
     private final S3UploadService uploadService;
 
     public void saveAdvertisement(SaveAdvertisementRequestDTO request, MultipartFile image) {
+        validateAlreadyExistAtDisplayOrder(request.position(), request.displayOrder());
+
         String imageUrl = uploadService.uploadFile(image, IMAGE_PREFIX);
 
         Advertisement advertisement = Advertisement.of(imageUrl, request.position(), request.displayOrder(),
                 request.linkUrl(), request.title(), request.content());
-        advertisementRepository.save(advertisement);
+        adminAdvertisementRepository.save(advertisement);
     }
 
     public void deleteAdvertisement(Long advertisementId) {
-        deleteAdvertisement(advertisementRepository.getById(advertisementId));
+        deleteAdvertisement(adminAdvertisementRepository.getById(advertisementId));
+    }
+
+    private void validateAlreadyExistAtDisplayOrder(Position position, int displayOrder) {
+        if (adminAdvertisementRepository.existsByPositionAndDisplayOrder(position, displayOrder)) {
+            throw new DuplicateDisplayOrderException();
+        }
     }
 
     private void deleteAdvertisement(Advertisement advertisement) {
         uploadService.deleteFile(advertisement.getImageUrl());
-        advertisementRepository.delete(advertisement);
+        adminAdvertisementRepository.delete(advertisement);
     }
 }
--- src/main/java/com/prography/lighton/advertisement/admin/infrastructure/AdminAdvertisementRepository.java
+++ src/main/java/com/prography/lighton/advertisement/admin/infrastructure/AdminAdvertisementRepository.java
@@ -2,11 +2,14 @@ package com.prography.lighton.advertisement.admin.infrastructure;
 
 import com.prography.lighton.advertisement.admin.infrastructure.exception.NoSuchAdvertisementException;
 import com.prography.lighton.advertisement.common.domain.entity.Advertisement;
+import com.prography.lighton.advertisement.common.domain.entity.enums.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminAdvertisementRepository extends JpaRepository<Advertisement, Long> {
+
+    boolean existsByPositionAndDisplayOrder(Position position, int displayOrder);
 
     default Advertisement getById(Long advertisementId) {
         return findById(advertisementId)
