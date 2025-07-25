ec2d52619006fa92428e23987527da2e5ce9b500
msk226 <kminseok5167@gmail.com>
2025-07-25 11:10:55 +0900

    feature: 버스킹 등록 요청 DTO 내 불필요한 버스킹 증빙자료 URL 필드 삭제

--- src/main/java/com/prography/lighton/performance/users/presentation/dto/request/SaveUserBuskingRequest.java
+++ src/main/java/com/prography/lighton/performance/users/presentation/dto/request/SaveUserBuskingRequest.java
@@ -3,7 +3,6 @@ package com.prography.lighton.performance.users.presentation.dto.request;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.URL;
 
 public record SaveUserBuskingRequest(
 
@@ -14,10 +13,6 @@ public record SaveUserBuskingRequest(
         @NotNull(message = "공연 일정 정보는 필수입니다.")
         @Valid
         ScheduleDTO schedule,
-
-        @NotBlank(message = "공연 증빙 자료 URL은 필수입니다.")
-        @URL(message = "공연 증빙 자료는 올바른 URL 형식이어야 합니다.")
-        String proof,
 
         @NotBlank(message = "아티스트 이름은 필수입니다.")
         String artistName,
