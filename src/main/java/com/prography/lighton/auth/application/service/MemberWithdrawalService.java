2ef5b613683be665ae5bef6cb58a34aecd2a1c52
msk226 <kminseok5167@gmail.com>
2025-08-05 20:43:15 +0900

    refactor: 회원 탈퇴 시, repository를 통해 조회한 회원을 통해 로직 수행

--- src/main/java/com/prography/lighton/auth/application/service/MemberWithdrawalService.java
+++ src/main/java/com/prography/lighton/auth/application/service/MemberWithdrawalService.java
@@ -2,9 +2,9 @@ package com.prography.lighton.auth.application.service;
 
 import com.prography.lighton.artist.users.application.service.ArtistService;
 import com.prography.lighton.member.common.domain.entity.Member;
-import com.prography.lighton.member.users.application.ManagePreferredGenreUseCase;
-import com.prography.lighton.member.users.infrastructure.repository.MemberRepository;
-import com.prography.lighton.member.users.infrastructure.repository.TemporaryMemberRepository;
+import com.prography.lighton.member.common.infrastructure.repository.MemberRepository;
+import com.prography.lighton.member.common.infrastructure.repository.TemporaryMemberRepository;
+import com.prography.lighton.member.users.application.UserMemberCommandService;
 import com.prography.lighton.performance.users.application.service.UserPerformanceLikeService;
 import com.prography.lighton.performance.users.application.service.UserPerformanceService;
 import lombok.RequiredArgsConstructor;
@@ -20,18 +20,17 @@ public class MemberWithdrawalService {
     private final TemporaryMemberRepository temporaryMemberRepository;
     private final UserPerformanceService userPerformanceService;
     private final UserPerformanceLikeService userPerformanceLikeService;
-    private final ManagePreferredGenreUseCase managePreferredGenreUseCase;
+    private final UserMemberCommandService userMemberCommandService;
     private final ArtistService artistService;
 
 
     @Transactional
     public void withdraw(Member member) {
         Member dbMember = memberRepository.getMemberById(member.getId());
-        member.withdraw();
-        memberRepository.flush();
+        dbMember.withdraw();
 
         userPerformanceService.inactivateAllByMember(dbMember);
-        managePreferredGenreUseCase.inactivateAllByMember(dbMember);
+        userMemberCommandService.inactivateAllByMember(dbMember);
         userPerformanceLikeService.inactivateAllByMember(dbMember);
         artistService.inactiveByMember(dbMember);
 
