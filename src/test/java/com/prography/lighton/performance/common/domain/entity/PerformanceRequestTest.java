fc1d9fc11344f94e8be188e356f4664ff1575a00
yooookm <yookyungmi02@gmail.com>
2025-08-12 02:26:39 +0900

    test: 공연 신청 관련 테스트코드 추가 - 공연 취소 시 권한 검증 테스트 코드 추가 - 공연 취소 시 취소 좌석수가 예약 좌석수보다 많을 시 검증 테스트코드 추가

--- src/test/java/com/prography/lighton/performance/common/domain/entity/PerformanceRequestTest.java
+++ src/test/java/com/prography/lighton/performance/common/domain/entity/PerformanceRequestTest.java
@@ -2,9 +2,12 @@ package com.prography.lighton.performance.common.domain.entity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
 
+import com.prography.lighton.member.common.domain.entity.Member;
 import com.prography.lighton.performance.common.domain.entity.enums.ApproveStatus;
 import com.prography.lighton.performance.common.domain.entity.fixture.PerformanceFixture;
+import com.prography.lighton.performance.common.domain.exception.NotAuthorizedPerformanceRequestException;
 import com.prography.lighton.performance.common.domain.exception.PerformanceNotApprovedException;
 import com.prography.lighton.performance.users.application.exception.BadPerformanceRequestException;
 import com.prography.lighton.performance.users.application.exception.NotEnoughSeatsException;
@@ -79,17 +82,34 @@ class PerformanceRequestTest {
         assertThat(p.getSeatInventory().getBookedSeatCount()).isEqualTo(0);
     }
 
-//    @Test
-//    @DisplayName("공연 취소시 bookedSeatCount가 0 아래로 내려가면 0으로 고정된다")
-//    void should_floor_bookedSeatCount_at_zero() {
-//        Performance p = PerformanceFixture.defaultPerformance();
-//        p.managePerformanceApplication(ApproveStatus.APPROVED);
-//
-//        int applySeat = 2;
-//        int wrongCancelSeat = 5;
-//        PerformanceRequest request =p.createRequest(applySeat, PerformanceFixture.defaultMember());
-//
-//        p.cancelRequest(wrongCancelSeat);
-//        assertThat(p.getSeatInventory().getBookedSeatCount()).isZero();
-//    }
+    @Test
+    @DisplayName("공연 취소시 취소 좌석수가 예약 좌석수보다 많으면 에러가 발생한다.")
+    void should_throw_cancelSeatCount_is_more_then_bookedSeatCount() {
+        Member member = PerformanceFixture.defaultMember();
+        Performance p = PerformanceFixture.defaultPerformance();
+        p.managePerformanceApplication(ApproveStatus.APPROVED);
+
+        int applySeat = 2;
+        int wrongCancelSeat = 5;
+        PerformanceRequest request = p.createRequest(applySeat, member);
+        ReflectionTestUtils.setField(request, "requestedSeats", wrongCancelSeat);
+
+        assertThatThrownBy(() -> p.cancelRequest(request, member))
+                .isInstanceOf(NotEnoughSeatsException.class);
+    }
+
+    @Test
+    @DisplayName("공연 취소시 공연 신청자와 공연 취소 요청자가 일치하지 않으면 에러가 발생한다.")
+    void should_throw_is_not_same_request_member() {
+        Member member = PerformanceFixture.defaultMember();
+        Performance p = PerformanceFixture.defaultPerformance();
+        p.managePerformanceApplication(ApproveStatus.APPROVED);
+
+        int applySeat = 2;
+        PerformanceRequest request = p.createRequest(applySeat, member);
+        Member wrongMember = mock(Member.class);
+
+        assertThatThrownBy(() -> p.cancelRequest(request, wrongMember))
+                .isInstanceOf(NotAuthorizedPerformanceRequestException.class);
+    }
 }
