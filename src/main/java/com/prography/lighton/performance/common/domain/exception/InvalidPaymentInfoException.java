cc382e18a67f15a47193571ab8448051be9a2b13
yooookm <yookyungmi02@gmail.com>
2025-08-12 02:09:17 +0900

    refactor: InvalidPerformanceException 생성 - 공연 관련 invalid exception은 모두 InvalidPerformanceException 상속

--- src/main/java/com/prography/lighton/performance/common/domain/exception/InvalidPaymentInfoException.java
+++ src/main/java/com/prography/lighton/performance/common/domain/exception/InvalidPaymentInfoException.java
@@ -1,8 +1,6 @@
 package com.prography.lighton.performance.common.domain.exception;
 
-import com.prography.lighton.common.exception.base.InvalidException;
-
-public class InvalidPaymentInfoException extends InvalidException {
+public class InvalidPaymentInfoException extends InvalidPerformanceException {
 
     private static final String MESSAGE = "유료 공연일 경우 계좌 정보, 은행명, 예금주, 금액은 모두 필수입니다.";
 
--- src/main/java/com/prography/lighton/performance/common/domain/exception/InvalidScheduleException.java
+++ src/main/java/com/prography/lighton/performance/common/domain/exception/InvalidScheduleException.java
@@ -1,8 +1,6 @@
 package com.prography.lighton.performance.common.domain.exception;
 
-import com.prography.lighton.common.exception.base.InvalidException;
-
-public class InvalidScheduleException extends InvalidException {
+public class InvalidScheduleException extends InvalidPerformanceException {
 
     private static final String MESSAGE = "유효하지 않은 날짜/시간입니다.";
 
