4ce613ad8286ae31c1bd542f440d105b48d170bd
yooookm <yookyungmi02@gmail.com>
2025-08-12 02:16:37 +0900

    feat: 공연 권한 관련 커스텀 익셉션은 NotAuthorizedException 상속

--- src/main/java/com/prography/lighton/performance/common/domain/exception/NotAuthorizedPerformanceRequestException.java
+++ src/main/java/com/prography/lighton/performance/common/domain/exception/NotAuthorizedPerformanceRequestException.java
@@ -1,16 +1,11 @@
 package com.prography.lighton.performance.common.domain.exception;
 
-import com.prography.lighton.common.utils.ApiUtils;
-import org.springframework.http.HttpStatus;
+import com.prography.lighton.common.exception.base.NotAuthorizedException;
 
-public class NotAuthorizedPerformanceRequestException extends RuntimeException {
-    private final String message = "이 공연 신청에 대한 권한이 없습니다.";
+public class NotAuthorizedPerformanceRequestException extends NotAuthorizedException {
+    private static final String message = "이 공연 신청에 대한 권한이 없습니다.";
 
-    public ApiUtils.ApiResult<?> body() {
-        return ApiUtils.error(HttpStatus.FORBIDDEN, message);
-    }
-
-    public HttpStatus status() {
-        return HttpStatus.FORBIDDEN;
+    public NotAuthorizedPerformanceRequestException() {
+        super(message);
     }
 }
