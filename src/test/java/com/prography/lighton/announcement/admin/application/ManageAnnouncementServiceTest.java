b32a5a58252cf54fa5e00eb9c417b322db056c1a
msk226 <kminseok5167@gmail.com>
2025-08-14 18:57:36 +0900

    test: 공지사항 관련 테스트 코드 작성

---
+++ src/test/java/com/prography/lighton/announcement/admin/application/ManageAnnouncementServiceTest.java
@@ -0,0 +1,87 @@
+package com.prography.lighton.announcement.admin.application;
+
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.ANNOUNCEMENT_ID;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.CONTENT;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.IMAGE_URL1;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.IMAGE_URL2;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.IMAGE_URL3;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.TITLE;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.createAnnouncement;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.prography.lighton.announcement.admin.infrastructure.AdminAnnouncementRepository;
+import com.prography.lighton.announcement.admin.presentation.dto.request.ManageAnnouncementRequestDTO;
+import com.prography.lighton.announcement.common.domain.entity.Announcement;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ManageAnnouncementServiceTest {
+
+    @Mock
+    AdminAnnouncementRepository adminAnnouncementRepository;
+
+
+    @Test
+    @DisplayName("공지사항을 등록할 수 있다")
+    void should_register_announcement() {
+        // Given
+        ManageAnnouncementService service = new ManageAnnouncementService(adminAnnouncementRepository);
+        ManageAnnouncementRequestDTO request = createRequest();
+        // When
+        service.registerAnnouncement(request);
+
+        // Then
+        verify(adminAnnouncementRepository).save(any(Announcement.class));
+    }
+
+    @Test
+    @DisplayName("공지사항을 수정할 수 있다")
+    void should_update_announcement() {
+        // Given
+        ManageAnnouncementService service = new ManageAnnouncementService(adminAnnouncementRepository);
+        Announcement announcement = createAnnouncement();
+        ManageAnnouncementRequestDTO request = createRequest();
+        when(adminAnnouncementRepository.getById(anyLong())).thenReturn(announcement);
+
+        // When
+        service.updateAnnouncement(ANNOUNCEMENT_ID, request);
+
+        // Then
+        verify(adminAnnouncementRepository).getById(ANNOUNCEMENT_ID);
+        assertThat(announcement.getTitle()).isEqualTo(TITLE);
+    }
+
+    @Test
+    @DisplayName("공지사항을 삭제할 수 있다")
+    void should_delete_announcement() {
+        // Given
+        ManageAnnouncementService service = new ManageAnnouncementService(adminAnnouncementRepository);
+        Announcement announcement = createAnnouncement();
+        when(adminAnnouncementRepository.getById(anyLong())).thenReturn(announcement);
+
+        // When
+        service.deleteAnnouncement(ANNOUNCEMENT_ID);
+
+        // Then
+        verify(adminAnnouncementRepository).getById(ANNOUNCEMENT_ID);
+        verify(adminAnnouncementRepository).delete(announcement);
+    }
+
+
+    private static ManageAnnouncementRequestDTO createRequest() {
+        return new ManageAnnouncementRequestDTO(
+                TITLE,
+                CONTENT,
+                List.of(IMAGE_URL1, IMAGE_URL2, IMAGE_URL3)
+        );
+    }
+}
---
+++ src/test/java/com/prography/lighton/announcement/common/domain/entity/AnnouncementTest.java
@@ -0,0 +1,65 @@
+package com.prography.lighton.announcement.common.domain.entity;
+
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.CONTENT;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.IMAGE_URL1;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.IMAGE_URL2;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.IMAGE_URL3;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.TITLE;
+import static com.prography.lighton.common.fixture.AnnouncementTestFixture.createAnnouncement;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.prography.lighton.announcement.common.domain.exception.InvalidAnnouncementException;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class AnnouncementTest {
+
+    @Test
+    @DisplayName("공지사항을 정상적으로 생성할 수 있다")
+    void should_create_announcement() {
+        // Given
+
+        // When
+        Announcement announcement = createAnnouncement();
+
+        // Then
+        assertThat(announcement.getTitle()).isEqualTo(TITLE);
+        assertThat(announcement.getContent()).isEqualTo(CONTENT);
+        assertThat(announcement.getImages()).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("공지사항의 이미지는 최대 3개까지만 허용된다")
+    void should_limit_images_to_three() {
+        // Given
+
+        // When & Then
+        assertThrows(InvalidAnnouncementException.class,
+                () -> Announcement.of(
+                        TITLE,
+                        CONTENT,
+                        List.of(IMAGE_URL1, IMAGE_URL2, IMAGE_URL3, "https://example.com/image4.jpg")
+                )
+        );
+    }
+
+    @Test
+    @DisplayName("공지사항을 수정할 수 있다")
+    void should_update_announcement() {
+        // Given
+        Announcement announcement = createAnnouncement();
+        String newTitle = "새로운 제목";
+        String newContent = "새로운 내용";
+        List<String> newImages = List.of("https://example.com/new_image1.jpg", "https://example.com/new_image2.jpg");
+
+        // When
+        announcement.update(newTitle, newContent, newImages);
+
+        // Then
+        assertThat(announcement.getTitle()).isEqualTo(newTitle);
+        assertThat(announcement.getContent()).isEqualTo(newContent);
+        assertThat(announcement.getImages()).containsExactlyElementsOf(newImages);
+    }
+}
