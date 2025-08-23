25d2d694123d658990a08a91a5762223753e888e
msk226 <kminseok5167@gmail.com>
2025-08-23 10:41:33 +0900

    feature: 비밀번호 검증 순서 변경

--- src/main/java/com/prography/lighton/member/common/domain/entity/vo/Password.java
+++ src/main/java/com/prography/lighton/member/common/domain/entity/vo/Password.java
@@ -24,12 +24,12 @@ public class Password {
     private static final Pattern PASSWORD_PATTERN = Pattern.compile(
             "^(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*()\\-_=+{}\\[\\]:;\"'<>,.?/\\\\|]).{8,20}$"
     );
-    
+
     @Column(nullable = false, name = "password")
     private String value;
 
     public static Password encodeAndCreate(String rawPassword, PasswordEncoder encoder) {
-        if (!PASSWORD_PATTERN.matcher(rawPassword).matches() || isBlank(rawPassword)) {
+        if (isBlank(rawPassword) || !PASSWORD_PATTERN.matcher(rawPassword).matches()) {
             throw new InvalidMemberException("비밀번호 형식이 올바르지 않습니다.");
         }
         return new Password(encoder.encode(rawPassword));
